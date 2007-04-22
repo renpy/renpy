1914df46b40e63d6f100f6d89df98625033c29a1
Tom Rothamel <pytom@bishoujo.us>
2007-04-22 16:08:58 +0000

    Added retag.

--- tag.py
+++ tag.py
@@ -4,4 +4,9 @@ import renpy
 import renpy.subprocess
 version = renpy.version[7:]
 
+import sys
+if "retag" in sys.argv[1:]:
+    renpy.subprocess.call([ "svn", "rm",  'http://www.bishoujo.us/svn/renpy/tags/' + version, "-m", "Tagging " + version ])
+    
+
 renpy.subprocess.call([ "svn", "copy", ".",  'http://www.bishoujo.us/svn/renpy/tags/' + version, "-m", "Tagging " + version ])
