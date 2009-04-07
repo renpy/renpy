6d40ced435a786b521030d64a4e957f7b9121504
Tom Rothamel <pytom@bishoujo.us>
2009-04-07 02:13:36 -0400

    Updated renpy.sh to work on the Mac again.

--- renpy.sh
+++ renpy.sh
@@ -3,11 +3,18 @@
 # We assume Darwin means Mac OS X. Sorry, Darwin guys.
 if [ "x`uname -s`" = "xDarwin" ]; then
     dir=`dirname "$0"`
-    dir=`cd $dir; pwd`
+    dir=`cd "$dir"; pwd`
     base=`basename "$0"`
 
-    export RENPY_LAUNCHER_DIR="`pwd`"
-    exec "${0%.sh}.app/Contents/MacOS/Ren'Py Launcher" "$dir/${base%.sh}.py" "$@"
+    export RENPY_LAUNCHER_DIR="$dir"
+
+    if [ -e "$dir/${base%.sh}.app/Contents/MacOS/${base%.sh}" ] ; then
+        launcher="$dir/${base%.sh}.app/Contents/MacOS/${base%.sh}"
+    else
+        launcher="$dir/${base%.sh}.app/Contents/MacOS/Ren'Py Launcher"
+    fi
+
+    exec "$launcher" "${0%.sh}.py" "$@"
 fi
 
-exec "`dirname \"$0\"`/lib/python" "-O" "${0%.sh}.py" "$@"
+exec "`dirname \"$0\"`/lib/python" "-OO" "${0%.sh}.py" "$@"
