db8adea23119a30b96fe49667a9a3a377f98275c
Tom Rothamel <pytom@bishoujo.us>
2011-07-16 10:55:00 -0400

    Remove all symlinks from tree.

---
+++ after_checkout.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -s ../help.html tutorial/README.html
+ln -s ../help.html the_question/README.html
+ln -s ../help.html template/README.html
+
+ln -s sphinx/source/license.rst LICENSE.txt
