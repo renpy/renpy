b65a5e6452ace6fbb8aa068c8cf3575eb37640c1
Tom Rothamel <pytom@bishoujo.us>
2011-07-17 14:39:10 -0400

    Redo build system to be simpler, and to automatically track dependencies.

---
+++ module/setuplib.py
@@ -0,0 +1,256 @@
+# This file encapsulates much of the complexity of the Ren'Py build process,
+# so setup.py can be clean by comparison.
+
+import os
+import sys
+import re
+import subprocess
+
+import distutils.core
+
+# This flag determines if we are compiling for Android or not.
+android = "RENPY_ANDROID" in os.environ
+
+# Note that the android build sets up CFLAGS for us, and ensures
+# that necessary libraries are present. So autoconfiguration is 
+# unnecessary on that platform.
+
+# The install variable is a list of directories that have Ren'Py
+# dependencies installed in them.
+if not android:
+    install = os.environ.get("RENPY_DEPS_INSTALL", "/usr")
+    install = install.split("::")
+    install = [ os.path.abspath(i) for i in install ]
+else:
+    install = [ ]
+    
+# The include and library dirs that we compile against.
+include_dirs = [ "." ]
+library_dirs = [ ]
+
+# Extra arguments that will be given to the compiler.
+extra_compile_args = [ ]
+extra_link_args = [ ]
+    
+def include(header, directory=None, optional=True):
+    """
+    Searches the install paths for `header`. If `directory` is given, we 
+    will append that to each of the install paths when trying to find 
+    the header. The directory the header is found in is added to include_dirs
+    if it's not present already. 
+    
+    `optional`
+        If given, returns False rather than abandoning the process.
+    """
+    
+    if android:
+        return True
+    
+    for i in install:
+        
+        if directory is not None:
+            idir = os.path.join(i, "include", directory)
+        else:
+            idir = os.path.join(i, "include")
+            
+        fn = os.path.join(idir, header)
+        
+        if os.path.exists(fn):
+            
+            if idir not in include_dirs:
+                include_dirs.append(idir)
+                
+            return True
+        
+    if optional:
+        return False
+    
+    if directory is None:
+        print "Could not find required header {0}.".format(header)
+    else:
+        print "Could not find required header {0}/{1}.".format(directory, header)
+
+    sys.exit(-1)
+    
+    
+def library(name, optional=False):
+    """
+    Searches for `library`. 
+    
+    `optional`
+        If true, this function will return False if a library is not found,
+        rather than reporting an error.
+    """
+    
+    if android:
+        return True
+    
+    for i in install:
+        
+        for ldir in [i, os.path.join(i, "lib") ]:
+            
+            for suffix in ( ".so", ".a", ".dylib" ):
+
+                fn = os.path.join(ldir, "lib" + name + suffix)
+                
+                if os.path.exists(fn):
+                    
+                    if ldir not in library_dirs:
+                        library_dirs.append(ldir)
+                        
+                    return True
+            
+    if optional:
+        return False
+    
+    print "Could not find required library {0}.".format(name)
+    sys.exit(-1)
+
+# A list of extension objects that we use.
+extensions = [ ]
+
+def cmodule(name, source, libs=[]):
+    """
+    Compiles the python module `name` from the files given in 
+    `source`, and the libraries in `libs`.
+    """
+    
+    extensions.append(distutils.core.Extension(
+        name,
+        source,
+        include_dirs=include_dirs,
+        library_dirs=library_dirs,
+        extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args,
+        libraries=libs,
+        ))
+
+def cython(name, source=[], libs=[]):
+    """
+    Compiles a cython module. This takes care of regenerating it as necessary
+    when it, or any of the files it depends on, changes.
+    """
+    
+    # Find the pyx file.
+    split_name = name.split(".")
+    module_name = split_name[-1]
+    
+    fn = "/".join(split_name) + ".pyx"
+    
+    if os.path.exists(os.path.join("..", fn)):
+        fn = os.path.join("..", fn)
+    elif os.path.exists(fn):
+        pass
+    else:
+        print "Could not find {0}.".format(fn)
+        sys.exit(-1)
+
+    module_dir = os.path.dirname(fn)
+                
+    # Figure out what it depends on.
+    deps = [ fn ]
+
+    f = file(fn)
+    for l in f:
+        
+        m = re.search(r'from\s*([\w.]+)\s*cimport', l)
+        if m:
+            deps.append(m.group(1).replace(".", "/") + ".pxd")
+            continue
+        
+        m = re.search(r'cimport\s*([\w.]+)', l)
+        if m:
+            deps.append(m.group(1).replace(".", "/") + ".pxd")
+            continue
+
+        m = re.search(r'include\s*"(.*?)"', l)
+        if m:
+            deps.append(m.group(1))
+            continue        
+    f.close()
+
+    # Filter out cython stdlib dependencies.
+    deps = [ i for i in deps if (not i.startswith("cpython/")) and (not i.startswith("libc/")) ]
+    
+    # Determine if any of the dependencies are newer than the c file.
+    c_fn = os.path.join("gen", module_name + ".c")
+    
+    if os.path.exists(c_fn):
+        c_mtime = os.path.getmtime(c_fn)
+    else:
+        c_mtime = 0
+        
+    out_of_date = False
+        
+    # print c_fn, "depends on", deps
+        
+    for dep_fn in deps:
+        
+        if os.path.exists(os.path.join(module_dir, dep_fn)):
+            dep_fn = os.path.join(module_dir, dep_fn)
+        elif os.path.exists(os.path.join("..", dep_fn)):
+            dep_fn = os.path.join("..", dep_fn)
+        elif os.path.exists(os.path.join("include", dep_fn)):
+            dep_fn = os.path.join("include", dep_fn)
+        elif os.path.exists(dep_fn):
+            pass
+        else:
+            print "{0} depends on {1}, which can't be found.".format(fn, dep_fn)
+            sys.exit(-1)
+
+        if os.path.getmtime(dep_fn) > c_mtime:
+            out_of_date = True
+
+    # If the file is out of date, regenerate it.
+    if out_of_date:
+        print name, "is out of date."
+
+        try:    
+            subprocess.check_call([
+                "cython",
+                "-Iinclude",
+                "-a",
+                fn,
+                "-o",
+                c_fn])
+
+        except subprocess.CalledProcessError, e:
+            print
+            print str(e)
+            print
+            sys.exit(-1)
+            
+    # Build the module normally once we have the c file.
+    cmodule(name, [ c_fn ] + source, libs=libs)
+
+
+py_modules = [ ]
+
+def pymodule(name):
+    """
+    Causes a python module to be included in the build.
+    """
+    
+    py_modules.append(name)
+
+
+def setup(name, version):
+    """
+    Calls the distutils setup function.
+    """
+
+    distutils.core.setup(
+        name = name,
+        version = version, 
+        ext_modules = extensions,
+        py_modules = py_modules,
+        )
+            
+# Ensure the gen directory exists.
+if not os.path.exists("gen"):
+    os.mkdir("gen")
+
+        
+    
+    
+        
--- run.sh
+++ run.sh
@@ -1,59 +1,10 @@
 # This builds out of date modules using the default C compiler, and then
 # runs them.
 
-# A sensible default for RENPY_DEPS_INSTALL
-export RENPY_DEPS_INSTALL=${RENPY_DEPS_INSTALL:-/usr}
-
 try () {
     "$@" || exit -1
 }
 
-# build <package> <module>
-# Builds the module in renpy/package named module.
-build () {
-
-    build=0
-
-    for i in renpy/$1/*.pxd; do
-        if [ $i -nt module/$2.c ]; then
-           build=1
-        fi
-    done
-        
-    if [ renpy/$1/$2.pyx -nt module/$2.c ]; then
-        build=1
-    fi
-
-    if [ $build != 0 ]; then    
-        echo renpy.$1.$2 is out of date.
-        try cython -Imodule -a renpy/$1/$2.pyx -o module/$2.c
-    fi
-}
-
-# Build the modules. To build a new module, it must be listed here
-# and in module/setup.py
-build display render
-build display accelerator
-
-build display gldraw
-build display gltexture
-build display glenviron
-build display glenviron_fixed
-build display glenviron_shader
-build display glenviron_limited
-build display glshader
-build display glrtt_copy
-build display glrtt_fbo
-
-build text ftfont
-build text textsupport
-build text texwrap
-
-echo Compiling...
-
-# Build the module, then come back here and run Ren'Py.
-try cd module
-try python setup.py install # > /dev/null
-try cd ..
+try python module/setup.py --quiet install
 
 exec ./renpy.py "$@"
