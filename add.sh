7b6da918a7961f1dd95e9571c55b3215e1b771ee
Tom Rothamel <pytom@bishoujo.us>
2012-04-22 19:25:28 -0400

    Distro fixes for the new host.

--- add.sh
+++ add.sh
@@ -5,7 +5,7 @@ if [ "x$1" = "x" ]; then
     exit
 fi
 
-rsync -av updates tom@onegeek.org:/home/tom/WWW.update/renpy/
+rsync -av updates tom@erika.onegeek.org:/home/tom/WWW.update/renpy/
 
 mkdir ~/ab/website/renpy/dl/$1
 cp dists/renpy-$1-* ~/ab/website/renpy/dl/$1
