b38ae1fb132f93f5fcce29e8671e431526a9dc8b
Tom Rothamel <pytom@bishoujo.us>
2012-06-05 03:30:24 -0400

    Add signed update scripts.

---
+++ scripts/generate_update_keys.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# Generate a pair of update keys.
+#
+
+import argparse
+import rsa
+
+ap = argparse.ArgumentParser()
+ap.add_argument("private")
+ap.add_argument("public")
+args = ap.parse_args()
+
+private, public = rsa.newkeys(2048)
+
+with open(args.public, "wb") as f:
+    f.write(public.save_pkcs1())
+
+with open(args.private, "wb") as f:
+    f.write(private.save_pkcs1())
+
---
+++ scripts/sign_update.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# Signs the update.json file.
+
+import argparse
+import rsa
+
+ap = argparse.ArgumentParser()
+ap.add_argument("private")
+ap.add_argument("json")
+args = ap.parse_args()
+
+with open(args.private, "rb") as f:
+    private = rsa.PrivateKey.load_pkcs1(f.read())
+    
+with open(args.json, "rb") as f:
+    message = f.read()
+    
+signature = rsa.sign(message, private, "SHA-256")
+
+with open(args.json + ".sig", "wb") as f:
+    f.write(signature.encode("base64"))
+    
+
+
+
