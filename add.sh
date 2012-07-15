2a47037f7e9b9adaecfdbf67d1ccc756ee95a182
Tom Rothamel <pytom@bishoujo.us>
2012-07-14 20:44:29 -0400

    New way to upload to the server.

--- add.sh
+++ add.sh
@@ -1,15 +1,4 @@
 #!/bin/sh
 
-if [ "x$1" = "x" ]; then
-    echo need version
-    exit
-fi
-
-rsync -av updates tom@erika.onegeek.org:/home/tom/WWW.update/renpy/
-
-mkdir ~/ab/website/renpy/dl/$1
-cp dists/renpy-$1-* ~/ab/website/renpy/dl/$1
-cp renpy-ppc.zip ~/ab/website/renpy/dl/$1
-
 cd ~/ab/website
 ./upload.sh
