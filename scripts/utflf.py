eed02c98c5f19054f2dff40361f6625b715bbbb4
Tom Rothamel <pytom@bishoujo.us>
2012-07-18 23:12:59 -0400

    Add normalization script.

---
+++ scripts/utflf.py
@@ -0,0 +1,32 @@
+# Fixes line endings and adds UTF-8 BOM to all rpy files in a
+# directory.
+
+import codecs
+import sys
+
+def process(fn):
+    with open(fn, "rb") as f:
+        data = f.read()
+
+    data = data.decode("utf-8")
+    data = data.replace("\r", "")
+    data = data.replace(u"\ufeff", "")
+    data = u"\ufeff" + data
+    data = data.encode("utf-8")
+
+    with open(fn, "wb") as f:
+        f.write(data)
+    
+import os
+
+for directory, dirs, files in os.walk(sys.argv[1]):
+    for fn in files:
+        fn = os.path.join(directory, fn)
+
+        if not fn.endswith(".rpy"):
+            continue
+
+        print fn
+        process(fn)
+        
+        
