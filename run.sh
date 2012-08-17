4cc4cae8bb0bdea4057e8deac973b9fe80691316
Tom Rothamel <pytom@bishoujo.us>
2012-08-17 11:50:47 -0400

    Allow Ren'Py to be build in virtualenvs, as well as with PYTHONPATH set.

--- run.sh
+++ run.sh
@@ -7,11 +7,15 @@ try () {
     "$@" || exit -1
 }
 
-if [ -z "$PYTHONPATH" ] ; then
-    echo PYTHONPATH not set
+if [ -z "$PYTHONPATH" -a -z "$VIRTUAL_ENV" ] ; then
+    echo Neither PYTHONPATH nor VIRTUAL_ENV is set.
     exit 1
 fi
 
-try python module/setup.py --quiet build $RENPY_BUILD_ARGS install_lib -d "$PYTHONPATH"
-
+if [ -n "$PYTHONPATH" ]; then
+    try python module/setup.py --quiet build $RENPY_BUILD_ARGS install_lib -d "$PYTHONPATH"
+else
+    try python module/setup.py --quiet build $RENPY_BUILD_ARGS install
+fi
+    
 exec ./renpy.py "$@"
