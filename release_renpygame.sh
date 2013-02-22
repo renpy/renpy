3a981be8c6932358a2117c81881d3f4cf8cd2b4a
Tom Rothamel <pytom@bishoujo.us>
2013-02-21 21:01:18 -0500

    Delete obsolete scripts.

--- release_renpygame.sh
+++
@@ -1,1 +0,0 @@
-zip -r renpygame-$1.zip renpygame-demo -x \*.svn\* -x \*saves\* -x \*.pyc -x \*.pyo -x \*~
--- upload.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-rsync -av updates tom@erika.onegeek.org:/home/tom/WWW.update/renpy/
