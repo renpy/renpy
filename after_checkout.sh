a69bb2edfa3926c1da7fe52ae21bd30d27a527a6
Tom Rothamel <pytom@bishoujo.us>
2013-10-31 22:17:58 -0400

    Fix after_checkout.sh.

--- after_checkout.sh
+++ after_checkout.sh
@@ -4,7 +4,7 @@ ROOT="$(dirname $(python -c "import os;print(os.path.realpath('$0'))"))"
 
 ln -s "$ROOT/help.html" "$ROOT/tutorial/README.html"
 ln -s "$ROOT/help.html" "$ROOT/the_question/README.html"
-ln -s "$ROOT/help.html" "$ROOT/template/README.html"
+ln -s "$ROOT/help.html" "$ROOT/templates/english/README.html"
 
 ln -s "$ROOT/sphinx/source/license.rst" "$ROOT/LICENSE.txt"
 ln -s "$ROOT/sphinx/build/html" "$ROOT/doc"
