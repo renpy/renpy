afa4d561c6acf4062b7b202870e0132d0a2783a8
Tom Rothamel <pytom@bishoujo.us>
2013-11-29 15:45:31 -0500

    Bump version.

--- renpy/__init__.py
+++ renpy/__init__.py
@@ -34,9 +34,8 @@ except ImportError:
 # The tuple giving the version. This needs to be updated when
 # we bump the version.
 #
-# Be sure to change script_version in launcher/game/script_version.rpy.
 # Be sure to change config.version in tutorial/game/options.rpy.
-version_tuple = (6, 16, 2, vc_version)
+version_tuple = (6, 16, 3, vc_version)
 
 # A verbose string computed from that version.
 version = "Ren'Py " + ".".join(str(i) for i in version_tuple)
