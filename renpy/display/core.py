renpy/renpy

commit dd4d3410a6c7a26878f7b204b6b5aa2872134bf0

Adjust times for children of tranformed layers.
If we don't do this, the children will be shown at the timebase
of the layer at_list, which cause huge problems if the children
themselves are being transformed.

renpy/display/core.py
@@ -911,6 +911,8 @@ class SceneLists(renpy.object.Object):
         if at_list:
             for a in at_list:
 
+                rv = renpy.display.layout.AdjustTimes(rv, None, None)
+
                 if isinstance(a, renpy.display.motion.Transform):
                     rv = a(child=rv)
                 else:

commit 7b3072983f77bad59851e0fd3cae2a1e59fc82a5

Fix regression in times for children of transformed layers.
The regression was introduced by dd4d3410a6c7a26878f7b204b6b5aa2872134bf0,
and shows up in the Sunrider tutorial battle, when the PACT ships
attack with missiles.

The problem was that we were adjusting the times improperly - using
AdjustTimes to make the adjustment. Instead, we want the MultiBox
containing the layer to make the adjustment (which it does when
layer_name is set). This is because the MultiBox handles start
and animation times that are not the time at which the layer
was shown.

renpy/display/core.py
@@ -906,24 +906,26 @@ class SceneLists(renpy.object.Object):
 
         rv = renpy.display.layout.MultiBox(layout='fixed', focus=layer, **properties)
         rv.append_scene_list(self.layers[layer])
+        rv.layer_name = layer
 
         time, at_list = self.layer_at_list[layer]
 
         if at_list:
-            for a in at_list:
 
-                rv = renpy.display.layout.AdjustTimes(rv, None, None)
+            for a in at_list:
 
                 if isinstance(a, renpy.display.motion.Transform):
                     rv = a(child=rv)
                 else:
                     rv = a(rv)
 
-                f = renpy.display.layout.MultiBox(layout='fixed')
-                f.add(rv, time, time)
-                rv = f
+            f = renpy.display.layout.MultiBox(layout='fixed')
+            f.add(rv, time, time)
+            f.layer_name = layer
 
-        rv.layer_name = layer
+            rv = f
+
+
         return rv
 
     def remove_hide_replaced(self, layer, tag):