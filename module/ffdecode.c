2be06386025b36735f8f631a47172bd102933171
Tom Rothamel <pytom@bishoujo.us>
2014-12-22 21:52:47 -0500

    Fix build on iOS.

--- module/ffdecode.c
+++ module/ffdecode.c
@@ -22,6 +22,7 @@
 #include <math.h>
 #include <limits.h>
 #include <libavutil/avstring.h>
+#include <libavutil/time.h>
 #include <libavformat/avformat.h>
 #include <libavcodec/avcodec.h>
 #include <libswscale/swscale.h>
--- module/setup.py
+++ module/setup.py
@@ -120,7 +120,7 @@ if has_fribidi and not android:
 # Sound.
 pymodule("pysdlsound.__init__")
 
-if not (android or ios):
+if not android:
 
     sound = [ "avformat", "avcodec", "avutil", "z" ]
     macros = [ ]
