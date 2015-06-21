893a27fca473e9ec3c188d14acbdfb03fd5782b7
Tom Rothamel <pytom@bishoujo.us>
2015-06-21 12:35:52 -0400

    Clean RAPT, rather than rebuilding it.
    This generally speeds up the distro process.

--- distribute.py
+++ distribute.py
@@ -113,37 +113,20 @@ def main():
             print "Compiling", i
             subprocess.check_call(["./renpy.sh", i, "quit" ])
 
-
     # Kick off the rapt build.
-    if not args.fast and not args.no_rapt:
-        out = open("/tmp/rapt_build.txt", "wb")
+    if not args.fast:
 
-        print("Building RAPT.")
+        print "Cleaning RAPT."
 
-        android = os.path.abspath("android")
+        sys.path.insert(0, os.path.join(ROOT, "rapt", "buildlib"))
 
-        rapt_cmd = [
-            os.path.join(android, "build_renpy.sh"),
-            "renpy",
-            ROOT,
-            ]
+        import rapt.interface # @UnresolvedImport
+        import rapt.build # @UnresolvedImport
 
-        if args.pygame:
-            rapt_cmd.append(args.pygame)
+        interface = rapt.interface.Interface()
+        rapt.build.distclean(interface)
 
-        rapt_build = subprocess.Popen(
-            rapt_cmd,
-            cwd = android,
-            stdout=out,
-            stderr=out)
-
-        code = rapt_build.wait()
-
-        if code:
-            print "RAPT build failed. The output is in /tmp/rapt_build.txt."
-            sys.exit(1)
-        else:
-            print "RAPT build succeeded."
+        print "Compiling RAPT and renios."
 
         compileall.compile_dir("rapt/buildlib/", ddir="rapt/buildlib/", quiet=1)
         compileall.compile_dir("renios/buildlib/", ddir="renios/buildlib/", quiet=1)
