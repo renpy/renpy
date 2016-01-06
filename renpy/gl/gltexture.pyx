e680702e87b85a767d26748037fa52755b0b45b5
Tom Rothamel <pytom@bishoujo.us>
2016-01-05 22:19:14 -0500

    Fix type error.
    -1 is not a valid value for an unsigned int, and since 0 is an
    invalid texture name, we use that instead.
    
    Fixes #778. (Well, not really, but we run on the new cython.)

--- renpy/gl/gltexture.pyx
+++ renpy/gl/gltexture.pyx
@@ -257,7 +257,7 @@ cdef class TextureCore:
         # The number of the OpenGL texture this texture object
         # represents.
         self.generation = 0
-        self.number = -1
+        self.number = 0
 
         # The format of this texture in the GPU (or 0 if not known).
         self.format = 0
@@ -449,7 +449,7 @@ cdef class TextureCore:
 
         cdef unsigned int texnums[1]
 
-        if self.number != -1:
+        if self.number != 0:
             return 0
 
         glGenTextures(1, texnums)
@@ -470,16 +470,18 @@ cdef class TextureCore:
 
         global total_texture_size
 
-        if self.number == -1:
+        if self.number == 0:
             return
 
         cdef GLuint texnums[1]
 
         texnums[0] = self.number
         glDeleteTextures(1, texnums)
+        self.number = 0
 
         texture_numbers.discard(self.number)
         total_texture_size -= self.width * self.height * 4
+
 
 class Texture(TextureCore):
     """
