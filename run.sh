0b6b601109bafbece3a484d334b03a1c42ffb0d3
Tom Rothamel <pytom@bishoujo.us>
2016-03-19 10:58:59 -0400

    Allow run.sh to be called from outside the Ren'Py directory.

--- run.sh
+++ run.sh
@@ -24,19 +24,19 @@ if [ -z "$PYTHONPATH" -a -z "$VIRTUAL_ENV" ] ; then
     exit 1
 fi
 
+ROOT="$(dirname $(realpath $0))"
+
 setup () {
     if [ -n "$PYTHONPATH" ]; then
         try python $1 $QUIET \
-            build -b build/lib.$variant -t build/tmp.$variant \
+            build -b $ROOT/build/lib.$variant -t $ROOT/build/tmp.$variant \
             $RENPY_BUILD_ARGS install_lib -d "$PYTHONPATH"
     else
         try python $1 $QUIET \
-            build -b build/lib.$variant -t build/tmp.$variant \
+            build -b $ROOT/build/lib.$variant -t $ROOT/build/tmp.$variant \
             $RENPY_BUILD_ARGS install
     fi
 }
-
-ROOT="$(dirname $(realpath $0))"
 
 if [ -e "$ROOT/pygame_sdl2" ]; then
     setup "$ROOT/pygame_sdl2/setup.py"
@@ -47,5 +47,5 @@ setup "$ROOT/module/setup.py"
 if  [ "$1" = "--build" ] ; then
     echo "Ren'Py build complete."
 else
-    exec python -O ./renpy.py "$@"
+    exec python -O $ROOT/renpy.py "$@"
 fi
