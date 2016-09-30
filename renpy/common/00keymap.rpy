renpy/renpy

commit 55211b89c9e09c7ac0ce853d5ab79cb19840b429

mac: In the case of a macapp, write screenshots to the desktop.

renpy/common/00keymap.rpy
@@ -224,16 +224,8 @@ init -1600 python:
         import os
         import __main__
 
-        # Pick the directory to save into.
-        dest = config.renpy_base.rstrip("/")
-
-        # Guess if we're an OSX App.
-        if dest.endswith("/Contents/Resources/autorun"):
-            # Go up 4 directories.
-            dest = os.path.dirname(dest)
-            dest = os.path.dirname(dest)
-            dest = os.path.dirname(dest)
-            dest = os.path.dirname(dest)
+        if renpy.macapp:
+            dest = os.expanduser("~/Desktop")
 
         # Try to pick a filename.
         i = 1

commit 84f27b15b42f3d4e233af66f118f121e56867808

Fix regression in screenshots.
Caused by 55211b89c9e09c7ac0ce853d5ab79cb19840b429.

renpy/common/00keymap.rpy
@@ -224,6 +224,8 @@ init -1600 python:
         import os
         import __main__
 
+        dest = config.renpy_base
+
         if renpy.macapp:
             dest = os.expanduser("~/Desktop")
 