44cecf38669d6b0bc241264219b53c2e97f48804
Tom Rothamel <pytom@bishoujo.us>
2016-12-21 02:02:09 -0500

    Fix checksum creation.

--- scripts/checksums.py
+++ scripts/checksums.py
@@ -4,6 +4,7 @@ import argparse
 import hashlib
 import os
 
+
 def hash_all(f, hash_name, hash_func, directory):
     f.write("\n")
     f.write("# {}\n".format(hash_name))
@@ -27,12 +28,13 @@ def hash_all(f, hash_name, hash_func, directory):
         f.write("{} {}\n".format(h.hexdigest(), fn))
         print(h.hexdigest(), fn)
 
+
 def main():
     ap = argparse.ArgumentParser()
     ap.add_argument("directory")
     args = ap.parse_args()
 
-    hashes = [ ("md5", hashlib.md5) ] # , ("sha1", hashlib.sha1), ("sha256", hashlib.sha256) ]
+    hashes = [ ("md5", hashlib.md5), ("sha1", hashlib.sha1), ("sha256", hashlib.sha256) ]
 
     with open(os.path.join(args.directory, "checksums.txt"), "w") as f:
 
@@ -43,4 +45,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
