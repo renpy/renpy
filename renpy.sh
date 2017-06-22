a85cc4f3e33f2e3c0b4b07224cfec54886ac17ae
Tom Rothamel <pytom@bishoujo.us>
2017-06-22 10:04:40 -0400

    Support for running novel platforms (like Raspi).

--- renpy.sh
+++ renpy.sh
@@ -24,7 +24,9 @@ ROOT=$(cd "$ROOT"; pwd)
 BASEFILE=$(basename "$SCRIPT" .sh)
 
 if [ -z "$RENPY_PLATFORM" ] ; then
-    case "$(uname -s)-$(uname -m)" in
+    RENPY_PLATFORM="$(uname -s)-$(uname -m)"
+
+    case "$RENPY_PLATFORM" in
         Darwin-*)
             RENPY_PLATFORM="darwin-x86_64"
             ROOT1="$ROOT/../Resources/autorun"
@@ -34,17 +36,20 @@ if [ -z "$RENPY_PLATFORM" ] ; then
             RENPY_PLATFORM="linux-x86_64"
             ROOT1="$ROOT"
             ROOT2="$ROOT"
-                        ;;
+            ;;
         *-i*86)
             RENPY_PLATFORM="linux-i686"
             ROOT1="$ROOT"
             ROOT2="$ROOT"
             ;;
+        Linux-*)
+            RENPY_PLATFORM="linux-$(uname -m)"
+            ROOT1="$ROOT"
+            ROOT2="$ROOT"
+            ;;
         *)
-            echo "Ren'Py could not detect that platform it's running on. Please set"
-            echo "the RENPY_PLATFORM environment variable to one of \"linux-i686\" or"
-            echo "\"linux-x86_64\", or \"darwin-x86_64\" and run this command again."
-            exit 1
+            ROOT1="$ROOT"
+            ROOT2="$ROOT"
             ;;
     esac
 fi
@@ -64,9 +69,14 @@ for BASE in "$ROOT" "$ROOT1" "$ROOT2"; do
 done
 
 if ! test -d "$LIB"; then
-    echo "Ren'Py platform files not found in any of $ROOT, $ROOT1 or $ROOT2!"
+    echo "Ren'Py platform files not found in:"
+    echo
+    echo "$ROOT/lib/$RENPY_PLATFORM"
+    echo
     echo "Please compile the platform files using the instructions in README.md"
     echo "or point them to an existing installation using ./after_checkout.sh <path>."
+    echo
+    echo "Alternatively, please set RENPY_PLATFORM to a different platform."
     exit 1
 fi
 
