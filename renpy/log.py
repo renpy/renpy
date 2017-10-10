77ee0211f476eb4d278a106f7af94490a3291e62
Tom Rothamel <pytom@bishoujo.us>
2017-10-09 22:44:24 -0400

    Ensure the log initalizes correctly on Android.

--- renpy/log.py
+++ renpy/log.py
@@ -92,7 +92,7 @@ class LogFile(object):
             return False
 
         try:
-            base = os.environ.get("RENPY_LOG_BASE", renpy.config.logdir)
+            base = os.environ.get("RENPY_LOG_BASE", renpy.config.logdir or renpy.config.basedir)
             fn = os.path.join(base, self.name + ".txt")
 
             altfn = os.path.join(tempfile.gettempdir(), "renpy-" + self.name + ".txt")
