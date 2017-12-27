8b1689edf1af47ef960e7b13350545a9c4d4c2d3
Tom Rothamel <pytom@bishoujo.us>
2017-12-26 20:03:38 -0500

    Build docs as part of the release process.

--- scripts/autobuild.sh
+++ scripts/autobuild.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
 
+pushd sphinx
+./build.sh
+popd
+
 if git diff --name-only last-build | grep -q \.pyx; then
     $RENPY_BUILD_ALL
     git branch -d last-build
--- sphinx/upload_dev.sh
+++ sphinx/upload_dev.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rsync -a ../doc-web/ tom@erika.onegeek.org:/home/tom/WWW.renpyorg/dev-doc/html/
+rsync -va ../doc-web/ tom@erika.onegeek.org:/home/tom/WWW.renpyorg/dev-doc/html/
