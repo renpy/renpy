ba832e5e3d522d03d5c71eb2e4cb2859d60c80dd
Tom Rothamel <pytom@bishoujo.us>
2018-04-26 08:55:46 -0400

    Fix signature of render.
    Per #1442. Keeping open, so after 7.0 I can evaluate and see if
    the opaque optimization even matters anymore, or should be
    removed.

--- renpy/display/render.pyx
+++ renpy/display/render.pyx
@@ -572,7 +572,7 @@ PIXELLATE = 3
 
 cdef class Render:
 
-    def __init__(Render self, float width, float height, draw_func=None, layer_name=None, bint opaque=None): #@DuplicatedSignature
+    def __init__(Render self, float width, float height, draw_func=None, layer_name=None, bint opaque=False): #@DuplicatedSignature
         """
         Creates a new render corresponding to the given widget with
         the specified width and height.
