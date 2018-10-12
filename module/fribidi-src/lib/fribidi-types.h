19204d0f820998edcf1af039fe0004235a57ff6c
Tom Rothamel <pytom@bishoujo.us>
2018-10-12 01:41:52 -0400

    Force the size of FriBidiChr to 4 bytes on all problems.
    Fixes a problem where on windows it was corrupting RTL text into
    garbage.

--- module/fribidi-src/lib/fribidi-types.h
+++ module/fribidi-src/lib/fribidi-types.h
@@ -12,17 +12,17 @@
  *
  * Copyright (C) 2004 Sharif FarsiWeb, Inc.
  * Copyright (C) 2001,2002 Behdad Esfahbod
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library, in a file named COPYING; if not, write to the
  * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
@@ -127,7 +127,7 @@ typedef FRIBIDI_UINT16 fribidi_uint16;
 typedef FRIBIDI_UINT32 fribidi_uint32;
 typedef FRIBIDI_BOOLEAN fribidi_boolean;
 
-typedef FRIBIDI_UNICHAR FriBidiChar;
+typedef FRIBIDI_UINT32 FriBidiChar;
 typedef FRIBIDI_STR_INDEX FriBidiStrIndex;
 
 
