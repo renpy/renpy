97769864f5cb6d770443c78ba959cba77ef3295c
Shehriyar Qureshi <SShehriyar266@gmail.com>
2018-11-02 23:14:23 +0500

    Remove class and follow yoda condition

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,22 +10,22 @@ sys.path.insert(0, current_directory)
 import main
 
 
-class TestRenpyPath:
-    def test_renpy_path_to_renpy_base(self):
+def test_renpy_path_to_renpy_base():
 
-        # We're just doing what the function does and comparing the values.
-        # This test should fail if the function is changed and returns
-        # something else.
+    # We're just doing what the function does and comparing the values.
+    # This test should fail if the function is changed and returns
+    # something else.
 
-        # we change sys.argv to mimimc as if renpy is being run
-        sys.argv.pop(1)
-        sys.argv[0] = 'renpy.py'
+    # we change sys.argv to replicate normal renpy execution
+    sys.argv.pop(1)
+    sys.argv[0] = 'renpy.py'
 
-        renpy_base = main.path_to_renpy_base()
+    renpy_base = main.path_to_renpy_base()
 
-        # we use sys.path[0] as it is the expected result of function
-        assert renpy_base == sys.path[0]
+    # we use sys.path[0] as it is the expected result of function
+    assert renpy_base == sys.path[0]
 
-    def test_path_to_common(self):
-        assert main.path_to_common(sys.path[0]) == "{}/renpy/common".format(
-            sys.path[0])
+
+def test_path_to_common():
+    assert "{}/renpy/common".format(sys.path[0]) == main.path_to_common(
+        sys.path[0])
