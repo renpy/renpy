68fd9d45433e3992991d5ba5e3765f894e8bda46
Shehriyar Qureshi <SShehriyar266@gmail.com>
2018-11-03 06:48:51 +0500

    Update test implementation
    We were using sys.path[0] as our known output value, but it was
    not constant. Hence, it wasn't really a known output value.
    
    Switched to using os.path.dirname() which should be a good enough
    for us to equate our function results to, as it does what the
    function is doing and if the function is changed in a way such
    that it changes its output value, the test will pick it up.

--- tests/test_main.py
+++ tests/test_main.py
@@ -12,20 +12,18 @@ import main
 
 def test_renpy_path_to_renpy_base():
 
-    # We're just doing what the function does and comparing the values.
-    # This test should fail if the function is changed and returns
-    # something else.
-
-    # we change sys.argv to replicate normal renpy execution
-    sys.argv.pop(1)
-    sys.argv[0] = 'renpy.py'
-
     renpy_base = main.path_to_renpy_base()
 
-    # we use sys.path[0] as it is the expected result of function
-    assert renpy_base == sys.path[0]
+    # os.path.dirname removes the script name, which
+    # in these test cases, is path to pytest executable.
+    # So we're testing these functions using path to pytest
+    # which should give us constant results and known output
+    # value.
+    assert renpy_base == os.path.dirname(sys.argv[0])
 
 
 def test_path_to_common():
-    assert "{}/renpy/common".format(sys.path[0]) == main.path_to_common(
-        sys.path[0])
+    # This test just checks that the function didn't get changed for some
+    # reason
+    assert "{}/renpy/common".format(os.path.dirname(
+        sys.argv[0])) == main.path_to_common(os.path.dirname(sys.argv[0]))
