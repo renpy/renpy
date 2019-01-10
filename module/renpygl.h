de67f53409bac9a10aef180c749294d582e1ad8c
Tom Rothamel <pytom@bishoujo.us>
2019-01-09 22:54:53 -0500

    Include SDL_opengles2 when required.

--- module/renpygl.h
+++ module/renpygl.h
@@ -1,7 +1,18 @@
 #ifndef RENPY_GL_H
-#define RENPY_H_H
+#define RENPY_GL_H
 
-#include <SDL2/SDL_opengl.h>
+#include <SDL.h>
+
+#if defined(__IPHONEOS__) || defined(__ANDROID__)
+
+#include <SDL_opengles2.h>
+
+#else
+
+#include <SDL_opengl.h>
+
+#endif
+
 #undef environ
 
 #endif
