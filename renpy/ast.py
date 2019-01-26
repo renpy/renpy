4c59d32beae613d782a5c964e6e1be684fea356d
Tom Rothamel <pytom@bishoujo.us>
2019-01-26 10:13:46 -0500

    Add allow_duplicate_labels to be defined early.

--- renpy/ast.py
+++ renpy/ast.py
@@ -1954,7 +1954,7 @@ def get_namespace(store):
 
 # Config variables that are set twice - once when the rpy is first loaded,
 # and then again at init time.
-EARLY_CONFIG = { "save_directory" }
+EARLY_CONFIG = { "save_directory", "allow_duplicate_labels" }
 
 define_statements = [ ]
 
