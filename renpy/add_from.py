6be0f498e4d2ac48bec47e1779b902f4fe84ec34
Tom Rothamel <pytom@bishoujo.us>
2019-02-05 17:36:40 -0500

    Allow add_from to deal with calls to a local label.
    Fixes #1709.

--- renpy/add_from.py
+++ renpy/add_from.py
@@ -34,9 +34,6 @@ def report_missing(target, filename, position):
     """
     Reports that the call statement ending at `position` in `filename`
     is missing a from clause.
-
-    `target`
-        The string
     """
 
     missing[filename].append((position, target))
@@ -50,6 +47,8 @@ def generate_label(target):
     """
     Generate a reasonable and unique new label for a call to `target`.
     """
+
+    target = target.replace(".", "_")
 
     n = 0
 
@@ -117,5 +116,6 @@ def add_from():
             process_file(fn)
 
     return False
+
 
 renpy.arguments.register_command("add_from", add_from)
