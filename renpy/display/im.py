305215823da6492c3e2ebffea308fbe1f345beae
Tom Rothamel <tom@rothamel.us>
2019-04-15 00:23:46 -0400

    Revert "web: Remove threading from im.py"
    This reverts commit 3c12d7ddb5271e510e5eb784c171523ff8de6673.

--- renpy/display/im.py
+++ renpy/display/im.py
@@ -29,7 +29,7 @@ import renpy.display
 import math
 import zipfile
 import cStringIO
-#import threading
+import threading
 import time
 import io
 
@@ -92,21 +92,11 @@ class Cache(object):
         # False if this is not the first preload in this tick.
         self.first_preload_in_tick = True
 
-        class ConditionStub:
-            def __enter__(self): pass
-            def __exit__(self, type, value, traceback): pass
-            def notify(self): cache.preload_thread_main()
-            def acquire(self): pass
-            def release(self): pass
-            def notifyAll(self): pass
-            def wait(self): pass
         # A lock that must be held when updating the cache.
-        #self.lock = threading.Condition()
-        self.lock = ConditionStub()
+        self.lock = threading.Condition()
 
         # A lock that mist be held to notify the preload thread.
-        #self.preload_lock = threading.Condition()
-        self.preload_lock = ConditionStub()
+        self.preload_lock = threading.Condition()
 
         # Is the preload_thread alive?
         self.keep_preloading = True
@@ -122,9 +112,9 @@ class Cache(object):
         self.cache_limit = 0
 
         # The preload thread.
-        #self.preload_thread = threading.Thread(target=self.preload_thread_main, name="preloader")
-        #self.preload_thread.setDaemon(True)
-        #self.preload_thread.start()
+        self.preload_thread = threading.Thread(target=self.preload_thread_main, name="preloader")
+        self.preload_thread.setDaemon(True)
+        self.preload_thread.start()
 
         # Have we been added this tick?
         self.added = set()
@@ -177,14 +167,14 @@ class Cache(object):
             self.cache_limit = int(renpy.config.image_cache_size_mb * 1024 * 1024 // 4)
 
     def quit(self):  # @ReservedAssignment
-        #if not self.preload_thread.isAlive():
-        #    return
+        if not self.preload_thread.isAlive():
+            return
 
         with self.preload_lock:
             self.keep_preloading = False
             self.preload_lock.notify()
 
-        #self.preload_thread.join()
+        self.preload_thread.join()
 
         self.clear()
 
@@ -446,8 +436,7 @@ class Cache(object):
 
     def preload_thread_main(self):
 
-        #while self.keep_preloading:
-        if self.keep_preloading:
+        while self.keep_preloading:
 
             self.preload_lock.acquire()
             self.preload_lock.wait()
@@ -518,11 +507,11 @@ class Cache(object):
                         self.preload_blacklist.add(image)
 
     def add_load_log(self, filename):
+
         if not renpy.config.developer:
             return
 
-        #preload = (threading.current_thread() is self.preload_thread)
-        preload = True
+        preload = (threading.current_thread() is self.preload_thread)
 
         self.load_log.insert(0, (time.time(), filename, preload))
 
