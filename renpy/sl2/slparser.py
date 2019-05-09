renpy/renpy

commit b0ee6fe3920d681a70933a7d66a703960713d401

Raise indention error for misspelled indent after keyword

renpy/sl2/slparser.py
@@ -236,7 +236,7 @@ class Parser(object):
                     l.error('keyword argument %r appears more than once in a %s statement.' % (name, self.name))
 
                 target.tag = l.require(l.word)
-
+                l.expect_noblock(name)
                 return True
 
             if name not in self.keyword:
@@ -264,6 +264,8 @@ class Parser(object):
                     l.error("a non-constant keyword argument like '%s %s' is not allowed after a python block." % (name, expr))
 
             target.keyword.append((name, expr))
+
+            l.expect_noblock(name)
 
         if block_only:
             l.expect_eol()

commit 3b74892f65c8b6fb2ece6f12faa16287eabcd9f6

sl2: Properties on the first line do not prevent a block.
Fixes a regression introduced by b0ee6fe3920d681a70933a7d66a703960713d401.

renpy/sl2/slparser.py
@@ -225,7 +225,7 @@ class Parser(object):
         block = False
 
         # Parses a keyword argument from the lexer.
-        def parse_keyword(l, expect):
+        def parse_keyword(l, expect, first_line):
             name = l.word()
 
             if name is None:
@@ -265,7 +265,8 @@ class Parser(object):
 
             target.keyword.append((name, expr))
 
-            l.expect_noblock(name)
+            if not first_line:
+                l.expect_noblock(name)
 
         if block_only:
             l.expect_eol()
@@ -288,7 +289,7 @@ class Parser(object):
                     block = False
                     break
 
-                parse_keyword(l, 'expected a keyword argument, colon, or end of line.')
+                parse_keyword(l, 'expected a keyword argument, colon, or end of line.', True)
 
         # The index of the child we're adding to this statement.
         child_index = 0
@@ -350,10 +351,10 @@ class Parser(object):
                 l.revert(state)
 
                 if not l.eol():
-                    parse_keyword(l, "expected a keyword argument or child statement.")
+                    parse_keyword(l, "expected a keyword argument or child statement.", False)
 
                 while not l.eol():
-                    parse_keyword(l, "expected a keyword argument or end of line.")
+                    parse_keyword(l, "expected a keyword argument or end of line.", False)
 
     def add_positional(self, name):
         global parser