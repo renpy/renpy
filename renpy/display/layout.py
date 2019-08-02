797ef0fc25689c27fb08dc7c6349292d2e4739cf
Tom Rothamel <pytom@bishoujo.us>
2019-08-01 22:01:35 -0400

    fix: Duplicate the children of DynamicDisplayable.
    Fixes #1966, which causesd a Transform that was added to the
    tree of surfaces twice, then rendered in a way that wasn't
    cached, to cause a crash.

--- renpy/display/layout.py
+++ renpy/display/layout.py
@@ -1230,6 +1230,8 @@ class DynamicDisplayable(renpy.display.core.Displayable):
 
     nosave = [ 'child' ]
 
+    _duplicatable = True
+
     def after_setstate(self):
         self.child = None
 
@@ -1248,12 +1250,25 @@ class DynamicDisplayable(renpy.display.core.Displayable):
         self.args = args
         self.kwargs = kwargs
 
+    def _duplicate(self, args):
+        rv = self._copy(args)
+
+        if rv.child is not None and rv.child._duplicateable:
+            rv.child = rv.child._duplicate(args)
+
+        return rv
+
     def visit(self):
         return [ ]
 
     def update(self, st, at):
         child, redraw = self.function(st, at, *self.args, **self.kwargs)
         child = renpy.easy.displayable(child)
+
+        if child._duplicatable:
+            child = child._duplicate(self._args)
+            child._unique()
+
         child.visit_all(lambda c : c.per_interact())
 
         self.child = child
@@ -1871,6 +1886,7 @@ class Flatten(Container):
 
     def get_placement(self):
         return self.child.get_placement()
+
 
 class AlphaMask(Container):
     """
