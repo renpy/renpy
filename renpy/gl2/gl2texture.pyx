26e0f1617ee6e2e98ee25c87337fa0b5ac054caf
Tom Rothamel <pytom@bishoujo.us>
2019-08-29 21:49:33 -0400

    gl2: Use the reverse matrix to multiple rendered textures.

--- renpy/gl2/gl2texture.pyx
+++ renpy/gl2/gl2texture.pyx
@@ -377,6 +377,8 @@ cdef class GLTexture:
         # Project the child from virtual space to the screen space.
         cdef Matrix transform
         transform = renpy.display.matrix.texture_projection(cw, ch)
+        if what.reverse:
+            transform *= what.reverse
 
         # Set up the default modes.
         glEnable(GL_BLEND)
