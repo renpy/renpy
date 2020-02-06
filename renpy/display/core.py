renpy/renpy

commit 17b8f03429d9bfa46bde730a7b82caeb031f6009

py3: Simplify some of the use of ranges in renpy.display.core.

renpy/display/core.py
@@ -1156,7 +1156,7 @@ class SceneLists(renpy.object.Object):
         be displayed, or everything will be removed.
         """
 
-        for i in reversed(xrange(len(self.layers[layer]))):
+        for i in range(len(self.layers[layer]) - 1, 0, -1):
 
             sle = self.layers[layer][i]
 
@@ -1211,7 +1211,7 @@ class SceneLists(renpy.object.Object):
 
             # Have to iterate in reverse order, since otherwise
             # the indexes might change.
-            for i in reversed(xrange(len(self.layers[layer]))):
+            for i in range(len(self.layers[layer]) - 1, 0, -1):
                 self.hide_or_replace(layer, i, hide)
 
         self.at_list[layer].clear()

commit d6889d52fb1d41005b4d3f6fca1cdf254edf3138

py3: Fix reverse iteration.
This fixes a regression introduced in 17b8f03429d9bfa46bde730a7b82caeb031f6009.

renpy/display/core.py
@@ -1156,7 +1156,7 @@ class SceneLists(renpy.object.Object):
         be displayed, or everything will be removed.
         """
 
-        for i in range(len(self.layers[layer]) - 1, 0, -1):
+        for i in range(len(self.layers[layer]) - 1, -1, -1):
 
             sle = self.layers[layer][i]
 
@@ -1211,7 +1211,7 @@ class SceneLists(renpy.object.Object):
 
             # Have to iterate in reverse order, since otherwise
             # the indexes might change.
-            for i in range(len(self.layers[layer]) - 1, 0, -1):
+            for i in range(len(self.layers[layer]) - 1, -1, -1):
                 self.hide_or_replace(layer, i, hide)
 
         self.at_list[layer].clear()