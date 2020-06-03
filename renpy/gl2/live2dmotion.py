159ad0e0f27e0395d2d72aad2f8a19a4657d8367
Tom Rothamel <pytom@bishoujo.us>
2020-06-03 01:54:45 -0400

    Revert "live2d: Remove incorrect usage of fade factors in motion."
    This reverts commit cf0d05b6eb20188a7e577eb7be0bc9a85a264709.

--- renpy/gl2/live2dmotion.py
+++ renpy/gl2/live2dmotion.py
@@ -156,7 +156,17 @@ class Motion(object):
 
     def get(self, st):
 
+        fadein = 0.5
+        fadeout = 0.5
+
         st = st % self.duration
+
+        if st < fadein:
+            factor = st / fadeout
+        elif st > self.duration - fadeout:
+            factor = 1.0 - (st - (self.duration - fadeout)) / fadeout
+        else:
+            factor = 1.0
 
         rv = { }
 
@@ -167,7 +177,7 @@ class Motion(object):
             for i in segments:
                 if t < i.duration:
                     if k[0] == "Parameter":
-                        rv[k] = i.get(t)
+                        rv[k] = i.get(t) * factor
                     else:
                         rv[k] = i.get(t)
 
