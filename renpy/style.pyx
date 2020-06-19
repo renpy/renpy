929106910ac17e1dbf3cbf928e11aba4ae6f056a
Tom Rothamel <pytom@bishoujo.us>
2020-06-18 21:23:08 -0400

    Convert strings in .pyx to unicode.

--- renpy/style.pyx
+++ renpy/style.pyx
@@ -19,7 +19,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-from __future__ import print_function
+from __future__ import print_function, unicode_literals
 
 from cpython.ref cimport PyObject, Py_XDECREF
 from libc.string cimport memset
