3107c2b051799b0cb81a634fe97cf6cd10ee9093
Andykl <andy_kl@bk.ru>
2020-08-23 02:20:24 +0200

    Allow use raw strings in simple_expression

--- renpy/parser.py
+++ renpy/parser.py
@@ -1042,23 +1042,24 @@ class Lexer(object):
         if self.eol():
             return False
 
+        old_pos = self.pos
         c = self.text[self.pos]
 
-        # Allow unicode strings.
-        if c == 'u':
+        # Allow unicode and raw strings.
+        for mod in ('uU', 'rR'):
+            if c not in mod:
+                continue
+
             self.pos += 1
 
             if self.pos == len(self.text):
-                self.pos -= 1
+                self.pos = old_pos
                 return False
 
             c = self.text[self.pos]
 
-            if c not in ('"', "'"):
-                self.pos -= 1
-                return False
-
-        elif c not in ('"', "'"):
+        if c not in ('"', "'"):
+            self.pos = old_pos
             return False
 
         delim = c
