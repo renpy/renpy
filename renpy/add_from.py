fa5eb321db26ce8cfa6da5d04e62f729979ac533
Tom Rothamel <pytom@bishoujo.us>
2020-09-01 02:05:45 -0400

    Use binary IO when adding from to calls.
    Fixes #2340.

--- renpy/add_from.py
+++ renpy/add_from.py
@@ -78,8 +78,8 @@ def process_file(fn):
     edits = missing[fn]
     edits.sort()
 
-    with open(fn, "r", encoding="utf-8") as f:
-        data = f.read()
+    with open(fn, "rb") as f:
+        data = f.read().decode("utf-8")
 
     # How much of the input has been consumed.
     consumed = 0
@@ -95,8 +95,8 @@ def process_file(fn):
 
     output += data[consumed:]
 
-    with open(fn + ".new", "w", encoding="utf-8") as f:
-        f.write(output)
+    with open(fn + ".new", "wb") as f:
+        f.write(output.encode("utf-8"))
 
     try:
         os.unlink(fn + ".bak")
