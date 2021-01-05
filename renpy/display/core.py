renpy/renpy

commit 63769aa8fd2af8b437cfd188c90693b229caf219

Avoid using renderers that aren't allowed.
This fixes a performance warning when going from the GL renderer
to a GL2 game.

renpy/display/core.py
@@ -2196,27 +2196,31 @@ class Interface(object):
         if self.safe_mode:
             renderer = "sw"
 
-        if (renderer == "angle" or renderer == "angle2") and (not renpy.windows):
-            renderer = "auto"
-
         renpy.config.renderer = renderer
 
-        if renderer == "auto":
-
-            if renpy.android or renpy.ios or renpy.emscripten:
-                renderers = [ "gles" ]
-            elif renpy.windows:
-                renderers = [ "gl", "angle", "gles", "sw" ]
-            else:
-                renderers = [ "gl", "gles", "sw" ]
-
-            if renpy.config.gl2:
-
-                for i in [ "gles", "angle", "gl" ]:
-                    if i in renderers:
-                        renderers.insert(0, i + "2")
-
+        if renpy.android or renpy.ios or renpy.emscripten:
+            renderers = [ "gles" ]
+        elif renpy.windows:
+            renderers = [ "gl", "angle", "gles", "sw" ]
         else:
+            renderers = [ "gl", "gles", "sw" ]
+
+        if renpy.config.gl2:
+
+            found = False
+
+            for i in [ "gles", "angle", "gl" ]:
+                if i in renderers:
+                    name = i + "2"
+                    renderers.insert(0, name)
+
+                    if renderer == name:
+                        found = True
+
+            if not found:
+                renderer = "auto"
+
+        if renderer in renderers:
             renderers = [ renderer, "sw" ]
 
         draw_objects = { }

commit 3b5a9eec12375a792a0dc117331e1728640c88f7

Fix selection of renderers to better include gl2.
Fixes a regression introduced by 63769aa8fd2af8b437cfd188c90693b229caf219.

renpy/display/core.py
@@ -2171,20 +2171,22 @@ class Interface(object):
         else:
             renderers = [ "gl", "gles", "sw" ]
 
-        if renpy.config.gl2:
+        gl2_renderers = [ ]
 
-            found = False
+        for i in [ "gl", "angle", "gles" ]:
 
-            for i in [ "gles", "angle", "gl" ]:
-                if i in renderers:
-                    name = i + "2"
-                    renderers.insert(0, name)
+            if i in renderers:
+                gl2_renderers.append(i + "2")
 
-                    if renderer == name:
-                        found = True
+            if renpy.config.gl2:
+                renderers = gl2_renderers + renderers
 
-            if not found:
-                renderer = "auto"
+                if renderer not in gl2_renderers:
+                    renderer = "auto"
+
+            else:
+
+                renderers = renderers + gl2_renderers
 
         if renderer in renderers:
             renderers = [ renderer, "sw" ]