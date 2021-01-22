5d84076ff12be40f4acba7189c907699946a220e
Andy_kl <andy_kl@bk.ru>
2021-01-22 03:39:26 +0200

    Add name and version of the game to early config names

--- renpy/ast.py
+++ renpy/ast.py
@@ -2086,7 +2086,14 @@ def get_namespace(store):
 
 # Config variables that are set twice - once when the rpy is first loaded,
 # and then again at init time.
-EARLY_CONFIG = { "save_directory", "allow_duplicate_labels", "keyword_after_python", "steam_appid" }
+EARLY_CONFIG = {
+    "save_directory",
+    "allow_duplicate_labels",
+    "keyword_after_python",
+    "steam_appid",
+    "name",
+    "version",
+}
 
 define_statements = [ ]
 
