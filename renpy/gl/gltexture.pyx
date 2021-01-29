731460b9ea5ecf3c8dfbc054a06d5d1f8ecda01f
Tom Rothamel <pytom@bishoujo.us>
2021-01-28 23:36:40 -0500

    Fix borders in the gl1 renderer.
    Fixes #2625, which was reported with a patch by mgambrell.

--- renpy/gl/gltexture.pyx
+++ renpy/gl/gltexture.pyx
@@ -913,7 +913,7 @@ def texture_grid_from_surface(surf, transient):
     rv.rows, texrows = compute_tiling(height, max_size, fill_factor)
 
     for rv_row, texrow in zip(rv.rows, texrows):
-        border_top, _, border_bottom = rv_row
+        border_y, _, _ = rv_row
         y, height, texheight = texrow
 
         row = [ ]
@@ -921,12 +921,12 @@ def texture_grid_from_surface(surf, transient):
         colnum = 0
 
         for rv_col, texcol in zip(rv.columns, texcolumns):
-            border_left, _, border_right = rv_col
+            border_x, _, _ = rv_col
             x, width, texwidth = texcol
 
             tex = alloc_texture(texwidth, texheight)
             tex.load_surface(surf, x, y, width, height,
-                             border_left, border_top, border_right, border_bottom)
+                             border_x, border_y, border_x, border_y)
 
             row.append(tex)
 
