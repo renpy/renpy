b8aece362b1dbbfbf6e12c2a67fc12492d70fd4d
dogtopus <dogtopus@users.noreply.github.com>
2021-02-19 05:36:55 -0400

    Fix issues that prevented #2653 from working.
    Specifically:
    
    - Explicitly cast the Unicode characters to FT_ULong before doing bit ops.
    - Add the decoded value by 0x10000 to get the actual Unicode value.
    - Change the rest of Py_UNICODE castings to FT_ULong so the Unicode values don't get casted back to 16-bit when rendering.
    - Make the decode routine easier to read.

--- renpy/text/ftfont.pyx
+++ renpy/text/ftfont.pyx
@@ -545,13 +545,15 @@ cdef class FTFont:
             next_min_advance = 0
 
             if len_s > 1 and is_ucs2_surrogate(s[0]) == 2 and is_ucs2_surrogate(s[1]) == 1:
-                next_c = ((s[0] & 0b1111111111) << 10) | (s[1] & 0b1111111111)
+                next_c = ((<FT_ULong> s[0]) & 0b1111111111) << 10
+                next_c |= (<FT_ULong> s[1]) & 0b1111111111
+                next_c += 0x10000
                 vs_offset = 2
             else:
                 next_c = s[0]
                 vs_offset = 1
 
-            if len_s > vs_offset and is_vs(s[vs_offset]):
+            if is_ucs2_surrogate(s[0]) != 1 and len_s > vs_offset and is_vs(s[vs_offset]):
                 vs = s[vs_offset]
                 next_index = FT_Face_GetCharVariantIndex(face, next_c, vs)
             else:
@@ -575,13 +577,15 @@ cdef class FTFont:
 
             if i < len_s - 1:
                 if i < len_s - 2 and is_ucs2_surrogate(s[i + 1]) == 2 and is_ucs2_surrogate(s[i + 2]) == 1:
-                    next_c = ((s[i + 1] & 0b1111111111) << 10) | (s[i + 2] & 0b1111111111)
+                    next_c = ((<FT_ULong> s[i + 1]) & 0b1111111111) << 10
+                    next_c |= (<FT_ULong> s[i + 2]) & 0b1111111111
+                    next_c += 0x10000
                     vs_offset = 3
                 else:
                     next_c = s[i + 1]
                     vs_offset = 2
 
-                if i < len_s - vs_offset and is_vs(s[i + vs_offset]):
+                if is_ucs2_surrogate(s[i + 1]) != 1 and i < len_s - vs_offset and is_vs(s[i + vs_offset]):
                     vs = s[i + vs_offset]
                     next_index = FT_Face_GetCharVariantIndex(face, next_c, vs)
                 else:
@@ -645,7 +649,7 @@ cdef class FTFont:
             if glyph.character == 0x200b:
                 continue
 
-            index = FT_Get_Char_Index(face, <Py_UNICODE> glyph.character)
+            index = FT_Get_Char_Index(face, <FT_ULong> glyph.character)
             cache = self.get_glyph(index)
 
             bmx = <int> (glyph.x + .5) + cache.bitmap_left
@@ -720,7 +724,7 @@ cdef class FTFont:
             underline_x = x - glyph.delta_x_offset
             underline_end = x + <int> glyph.advance + expand
 
-            index = FT_Get_Char_Index(face, <Py_UNICODE> glyph.character)
+            index = FT_Get_Char_Index(face, <FT_ULong> glyph.character)
             cache = self.get_glyph(index)
 
             # with nogil used to be here, but it slowed things down.
