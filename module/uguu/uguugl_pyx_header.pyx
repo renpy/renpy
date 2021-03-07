0091838950576b8efd7cd53969030dc876f7d322
Tom Rothamel <pytom@bishoujo.us>
2021-03-07 13:31:38 -0500

    Report errors loading GL functions.

--- module/uguu/uguugl_pyx_header.pyx
+++ module/uguu/uguugl_pyx_header.pyx
@@ -1,4 +1,4 @@
-from sdl2 cimport SDL_GL_GetProcAddress
+from sdl2 cimport SDL_GL_GetProcAddress, SDL_GetError
 
 found_functions = set()
 
@@ -13,6 +13,8 @@ cdef void *find_gl_command(names):
             found_functions.add(names[0].decode("utf-8"))
             return rv
 
+    import renpy
+    renpy.display.log.write("UGUU couldn't find {}: {}".format(names[0], SDL_GetError()))
 
     return NULL
 
