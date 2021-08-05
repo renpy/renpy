renpy/renpy

commit d49cb97e19ce2d7c3b24dec1f1405d9539217d21

The main widget of a textbutton should be the button.

renpy/ui.py
@@ -991,7 +991,6 @@ def _textbutton(label, clicked=None, style=None, text_style=None, substitute=Tru
     rv = renpy.display.behavior.Button(style=style, clicked=clicked, **button_kwargs)
     text = renpy.text.text.Text(label, style=text_style, substitute=substitute, scope=scope, **text_kwargs)
     rv.add(text)
-    rv._main = text
     rv._composite_parts = [ text ]
     return rv
 

commit 05b89d189ef8613f811cecd37a5c90eb00f5b931

Store base displayables and main displayables separately.
Fixes #2959, a regression introduced by
d49cb97e19ce2d7c3b24dec1f1405d9539217d21 , by separating out the
base displayable that gets focus from the main displayable that
takes scope.

renpy/display/focus.py
@@ -283,7 +283,7 @@ def before_interact(roots):
     grab = replaced_by.get(id(grab), None)
 
     if override is not None:
-        d = renpy.exports.get_displayable(*override)
+        d = renpy.exports.get_displayable(base=True, *override)
 
         if (d is not None) and (current is not d) and not grab:
             current = d

renpy/display/screen.py
@@ -318,6 +318,7 @@ class ScreenDisplayable(renpy.display.layout.Container):
         self.children = [ ]
         self.transforms = { }
         self.widgets = { }
+        self.base_widgets = { }
         self.old_widgets = None
         self.old_transforms = None
         self.hidden_widgets = { }
@@ -362,6 +363,9 @@ class ScreenDisplayable(renpy.display.layout.Container):
 
         # A map from name to the widget with that name.
         self.widgets = { }
+
+        # Same, but to the widget without considering _main.
+        self.base_widgets = { }
 
         # The persistent cache.
         self.cache = { }
@@ -465,6 +469,7 @@ class ScreenDisplayable(renpy.display.layout.Container):
         rv = ScreenDisplayable(self.screen, self.tag, self.layer, self.widget_properties, self.scope, **self.properties)
         rv.transforms = self.transforms.copy()
         rv.widgets = self.widgets.copy()
+        rv.base_widgets = self.widgets.copy()
         rv.old_transfers = True
         rv.child = self.child
 
@@ -605,6 +610,7 @@ class ScreenDisplayable(renpy.display.layout.Container):
         self.old_widgets = self.widgets
         self.old_transforms = self.transforms
         self.widgets = { }
+        self.base_widgets = { }
         self.transforms = { }
 
         push_current_screen(self)
@@ -1278,7 +1284,7 @@ def current_screen():
     return _current_screen
 
 
-def get_displayable(screen, id, layer=None): # @ReservedAssignment
+def get_displayable(screen, id, layer=None, base=False): # @ReservedAssignment
     """
     :doc: screens
     :name: renpy.get_displayable
@@ -1305,7 +1311,11 @@ def get_displayable(screen, id, layer=None): # @ReservedAssignment
     if screen.child is None:
         screen.update()
 
-    rv = screen.widgets.get(id, None)
+    if base:
+        rv = screen.base_widgets.get(id, None)
+    else:
+        rv = screen.widgets.get(id, None)
+
     return rv
 
 

renpy/sl2/slast.py
@@ -909,6 +909,7 @@ class SLDisplayable(SLBlock):
 
                 if widget_id and not ctx.unlikely:
                     screen.widgets[widget_id] = main
+                    screen.base_widgets[widget_id] = d
 
                 if self.scope and main._uses_scope:
                     if copy_on_change:
@@ -945,6 +946,7 @@ class SLDisplayable(SLBlock):
 
                 if widget_id and not ctx.unlikely:
                     screen.widgets[widget_id] = main
+                    screen.base_widgets[widget_id] = d
                 # End child creation code.
 
                 imagemap = self.imagemap
@@ -1028,6 +1030,7 @@ class SLDisplayable(SLBlock):
 
                 if widget_id:
                     screen.widgets[widget_id] = main
+                    screen.base_widgets[widget_id] = d
                 # End child creation code.
 
                 cache.copy_on_change = False

renpy/ui.py
@@ -991,6 +991,7 @@ def _textbutton(label, clicked=None, style=None, text_style=None, substitute=Tru
     rv = renpy.display.behavior.Button(style=style, clicked=clicked, **button_kwargs)
     text = renpy.text.text.Text(label, style=text_style, substitute=substitute, scope=scope, **text_kwargs)
     rv.add(text)
+    rv._main = text
     rv._composite_parts = [ text ]
     return rv
 