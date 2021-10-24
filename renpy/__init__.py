eb2d34f1c6d5769edc7f09ebb3448315d46b9dd6
Tom Rothamel <pytom@bishoujo.us>
2021-10-24 00:23:24 -0400

    Bump version.

--- renpy/__init__.py
+++ renpy/__init__.py
@@ -81,10 +81,10 @@ official = official and getattr(site, "renpy_build_official", False)
 if PY2:
 
     # The tuple giving the version number.
-    version_tuple = (7, 4, 10, vc_version)
+    version_tuple = (7, 5, 0, vc_version)
 
     # The name of this version.
-    version_name = "Lucky Four-Footed Rabbit"
+    version_name = "TBD"
 
 else:
 
