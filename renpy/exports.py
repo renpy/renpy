renpy/renpy

commit d188403cfac55915733a4d8d2cb0b2a3d1492776

Sanitize values that Ren'Py will place into persistent.
Fixes #3260.

renpy/exports.py
@@ -757,7 +757,7 @@ def show(name, at_list=[ ], layer=None, what=None, zorder=None, tag=None, behind
         img._unique()
 
     # Update the list of images we have ever seen.
-    renpy.game.persistent._seen_images[name] = True # type: ignore
+    renpy.game.persistent._seen_images[(str(i) for i in name)] = True
 
     if tag and munge_name:
         name = (tag,) + name[1:]
@@ -2363,7 +2363,7 @@ def mark_label_seen(label):
     Marks the named label as if it has been already executed on the current user's
     system.
     """
-    renpy.game.persistent._seen_ever[label] = True # type: ignore
+    renpy.game.persistent._seen_ever[str(label)] = True # type: ignore
 
 
 def mark_label_unseen(label):
@@ -2440,7 +2440,7 @@ def mark_image_seen(name):
     if not isinstance(name, tuple):
         name = tuple(name.split())
 
-    renpy.game.persistent._seen_images[name] = True # type: ignore
+    renpy.game.persistent._seen_images[(str(i) for i in name)] = True
 
 
 def mark_image_unseen(name):

commit 7b1e58fa9ce320b828b4013a13af7302096ad006

Fix regression introduced by d188403cfac55915733a4d8d2cb0b2a3d1492776.
Whoops.

renpy/exports.py
@@ -757,7 +757,7 @@ def show(name, at_list=[ ], layer=None, what=None, zorder=None, tag=None, behind
         img._unique()
 
     # Update the list of images we have ever seen.
-    renpy.game.persistent._seen_images[(str(i) for i in name)] = True
+    renpy.game.persistent._seen_images[tuple(str(i) for i in name)] = True
 
     if tag and munge_name:
         name = (tag,) + name[1:]
@@ -2440,7 +2440,7 @@ def mark_image_seen(name):
     if not isinstance(name, tuple):
         name = tuple(name.split())
 
-    renpy.game.persistent._seen_images[(str(i) for i in name)] = True
+    renpy.game.persistent._seen_images[tuple(str(i) for i in name)] = True
 
 
 def mark_image_unseen(name):