e30bcf7fd6de6e0c0cee0675d69d2656039012e9
Tom Rothamel <pytom@bishoujo.us>
2022-02-08 01:58:09 -0500

    installer: Install vscode on windows.

--- launcher/game/installer.py
+++ launcher/game/installer.py
@@ -294,10 +294,15 @@ def processing(message, **kwargs):
 install_args = [ ]
 install_error = ""
 
-def run(*args):
+def run(*args, **kwargs):
     """
     Runs a program with the given arguments, in the target directory.
     """
+
+    environ = { renpy.exports.fsencode(k) : renpy.exports.fsencode(v) for k, v in os.environ.items() }
+
+    for k, v in kwargs.pop("environ", {}).items():
+        environ[renpy.exports.fsencode(k)] = renpy.exports.fsencode(v)
 
     global install_args
     global install_error
@@ -305,7 +310,7 @@ def run(*args):
     args = [ renpy.exports.fsencode(i) for i in args ]
 
     try:
-        subprocess.check_call(args, cwd=target)
+        subprocess.check_call(args, cwd=target, env=environ) # type: ignore
     except Exception as e:
         install_args = args
         install_error = str(e)
@@ -319,8 +324,9 @@ def main():
 
     if renpy.linux:
 
+
         # Download vscode.
-        download("https://code.visualstudio.com/sha/download?build=stable&os=linux-x64", "temp:vscode.tar.gz")
+        download("https://code.visualstudio.com/sha/download?build=stable&os=linux-x64", "temp:vscode-linux-x64.tar.gz")
 
         # Back up the data directory.
         remove("temp:vscode-data")
@@ -330,7 +336,7 @@ def main():
         # Unpack vscode.
         mkdir("vscode")
         remove("vscode/VSCode-linux-x64")
-        unpack("temp:vscode.tar.gz", "vscode")
+        unpack("temp:vscode-linux-x64.tar.gz", "vscode")
 
         # Restore the data directory.
 
@@ -341,4 +347,34 @@ def main():
 
         # Install the Ren'Py extension.
         processing(_("Installing the Ren'Py extension."))
-        run("vscode/VSCode-linux-x64/bin/code", "--install-extension", "LuqueDaniel.languague-renpy")
+        run("vscode/VSCode-linux-x64/code", "vscode/VSCode-linux-x64/resources/app/out/cli.js", "--ms-enable-electron-run-as-node",
+            "--install-extension", "LuqueDaniel.languague-renpy",
+            environ={ "VSCODE_DEV" : "", "ELECTRON_RUN_AS_NODE" : "1" })
+
+    elif renpy.windows:
+
+        # Download vscode.
+        download("https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-archive", "temp:vscode-win32-x64.tar.gz")
+
+        # Back up the data directory.
+        remove("temp:vscode-data")
+        if exists("vscode/VSCode-win32-x64/data"):
+            move("vscode/VSCode-win32-x64/data", "temp:vscode-data")
+
+        # Unpack vscode.
+        mkdir("vscode/VSCode-win32-x64")
+        remove("vscode/VSCode-win32-x64")
+        unpack("temp:vscode-win32-x64.tar.gz", "vscode/VSCode-win32-x64")
+
+        # Restore the data directory.
+
+        if exists("temp:vscode-data"):
+            move("temp:vscode-data", "vscode/VSCode-win32-x64/data")
+        else:
+            mkdir("vscode/VSCode-win32-x64/data")
+
+        # Install the Ren'Py extension.
+        processing(_("Installing the Ren'Py extension."))
+        run("vscode/VSCode-win32-x64/Code.exe", "vscode/VSCode-win32-x64/resources/app/out/cli.js", "--ms-enable-electron-run-as-node",
+            "--install-extension", "LuqueDaniel.languague-renpy",
+            environ={ "VSCODE_DEV" : "", "ELECTRON_RUN_AS_NODE" : "1" })
