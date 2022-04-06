722cc242aff8a1b7f9f9bbfa12b8517afdb7a16d
Gouvernathor <aanonyme.personne@hotmail.fr>
2022-04-06 14:30:48 +0200

    Correctly access Null
    fix #3392

--- renpy/display/viewport.py
+++ renpy/display/viewport.py
@@ -673,4 +673,4 @@ class VPGrid(Viewport):
             if not allow_underfull:
                 raise exc
             for _ in range(delta):
-                self.add(Null())
+                self.add(renpy.display.layout.Null())
