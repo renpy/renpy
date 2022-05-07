fa15f1212db9bfebf4d0c900b26776100daebbce
Tom Rothamel <pytom@bishoujo.us>
2022-05-07 01:46:56 -0400

    Modal windows/frames shouldn't set Render.modal.
    Render.modal is meant to optimize the case where things are globally
    modal - nothing below the displayable is considered by the focus
    system.
    
    Window's modal property is meant to prevent displayables behind the
    window from getting focus, while still allowing things behind the
    window to get focus.

--- renpy/display/layout.py
+++ renpy/display/layout.py
@@ -1350,7 +1350,6 @@ class Window(Container):
                 height = min(height, ymaximum)
 
         rv = renpy.display.render.Render(width, height)
-        rv.modal = self.style.modal
 
         # Draw the background. The background should render at exactly the
         # requested size. (That is, be a Frame or a Solid).
