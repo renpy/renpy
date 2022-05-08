bbf072cde7499d803b857fbc87b11a2533678142
Tom Rothamel <pytom@bishoujo.us>
2022-05-08 00:40:21 -0400

    Checkpoint when a random number is generated.
    This ensures that a rollback point will be created at the next
    statement, preventing random number generaton from being
    reverted when a save is loaded.
    
    Fixes #3479.

--- renpy/revertable.py
+++ renpy/revertable.py
@@ -462,6 +462,18 @@ class RevertableObject(object):
         self.__dict__.update(compressed)
 
 
+def checkpointing(method):
+
+    @_method_wrapper(method)
+    def do_checkpoint(self, *args, **kwargs):
+
+        renpy.game.context().force_checkpoint = True
+
+        return method(self, *args, **kwargs)
+
+    return do_checkpoint
+
+
 class RollbackRandom(random.Random):
     """
     This is used for Random objects returned by renpy.random.Random.
@@ -484,14 +496,14 @@ class RollbackRandom(random.Random):
     def _rollback(self, compressed):
         super(RollbackRandom, self).setstate(compressed)
 
-    setstate = mutator(random.Random.setstate)
+    setstate = checkpointing(mutator(random.Random.setstate))
 
     if PY2:
-        jumpahead = mutator(random.Random.jumpahead) # type: ignore
+        jumpahead = checkpointing(mutator(random.Random.jumpahead)) # type: ignore
 
-    getrandbits = mutator(random.Random.getrandbits)
-    seed = mutator(random.Random.seed) # type: ignore
-    random = mutator(random.Random.random)
+    getrandbits = checkpointing(mutator(random.Random.getrandbits))
+    seed = checkpointing(mutator(random.Random.seed)) # type: ignore
+    random = checkpointing(mutator(random.Random.random))
 
     def Random(self, seed=None):
         """
@@ -504,7 +516,6 @@ class RollbackRandom(random.Random):
         new = RollbackRandom()
         new.seed(seed)
         return new
-
 
 class DetRandom(random.Random):
     """
@@ -526,6 +537,8 @@ class DetRandom(random.Random):
 
         if log.current is not None:
             log.current.random.append(rv)
+
+        renpy.game.context().force_checkpoint = True
 
         return rv
 
