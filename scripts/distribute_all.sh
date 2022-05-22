a9f4c518f88105a2f295761d1d23db05a5907613
Tom Rothamel <pytom@bishoujo.us>
2022-05-22 12:58:50 -0400

    Check in release scripts.

---
+++ scripts/distribute_all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+cd $(dirname $(dirname $(readlink -f $0)))
+
+git status
+sleep 2
+
+(
+    set -e
+    . ~/.virtualenvs/renpy/bin/activate
+    cd sphinx
+    ./build.sh
+)
+
+nice rpy build --python 3
+nice rpy build --python 2
+
+nice lib/py3-linux-x86_64/python distribute.py
+nice lib/py2-linux-x86_64/python distribute.py
--- scripts/mac/mac_dmg_server.sh
+++ scripts/mac/mac_dmg_server.sh
@@ -31,7 +31,7 @@ echo "Submitted dmg for notarization."
 
 $(dirname $0)/wait_notarization.py
 
-sleep 15
+sleep 60
 
 xcrun stapler staple "$DMGBASE"
 
