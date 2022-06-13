83e20c8874d32106def48500da50b1fd74054104
Andy_kl <andy_kl@bk.ru>
2022-06-14 02:51:22 +0400

    Do not log_clock twice on andriod. StdoutRedirector writes in both real stdout and log file itself.

--- renpy/main.py
+++ renpy/main.py
@@ -42,11 +42,8 @@ last_clock = time.time()
 def log_clock(s):
     global last_clock
     now = time.time()
-    s = "{} took {:.2f}s".format(s, now - last_clock)
 
-    renpy.display.log.write(s)
-    if renpy.android and not renpy.config.log_to_stdout:
-        print(s)
+    print("{} took {:.2f}s".format(s, now - last_clock))
 
     # Pump the presplash window to prevent marking
     # our process as unresponsive by OS
