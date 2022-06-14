f1d9e1d44bc6bfbeac9470056cc1f076de99868f
Tom Rothamel <pytom@bishoujo.us>
2022-06-13 21:45:50 -0400

    Avoid using a PBO on ANGLE.
    Fixes #3654, where this was causing the image to not be fully
    loaded.

--- renpy/gl2/gl2texture.pyx
+++ renpy/gl2/gl2texture.pyx
@@ -415,13 +415,24 @@ cdef class GLTexture(GL2Model):
         # works around a bug in Samsung android devices running Android 11,
         # where glTexImage2D doesn't seem to work when the pixels are not
         # aligned.
-        glGenBuffers(1, &pixel_buffer)
-        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pixel_buffer)
-        glBufferData(GL_PIXEL_UNPACK_BUFFER, s.h * s.pitch, s.pixels, GL_STATIC_DRAW)
-        glPixelStorei(GL_UNPACK_ROW_LENGTH, s.pitch // 4)
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, self.width, self.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, <void *> 0)
-        glUnmapBuffer(GL_PIXEL_UNPACK_BUFFER)
-        glDeleteBuffers(1, &pixel_buffer)
+
+        # But it doesn't seem to work with ANGLE, so we avoid using PBOs when
+        # angle is in use.
+
+        if not draw.angle:
+
+            glGenBuffers(1, &pixel_buffer)
+            glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pixel_buffer)
+            glBufferData(GL_PIXEL_UNPACK_BUFFER, s.h * s.pitch, s.pixels, GL_STATIC_DRAW)
+            glPixelStorei(GL_UNPACK_ROW_LENGTH, s.pitch // 4)
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, self.width, self.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, <void *> 0)
+            glUnmapBuffer(GL_PIXEL_UNPACK_BUFFER)
+            glDeleteBuffers(1, &pixel_buffer)
+
+        else:
+            glPixelStorei(GL_UNPACK_ROW_LENGTH, s.pitch // 4)
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, self.width, self.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, s.pixels)
+
 
         mesh = Mesh2.texture_rectangle(-1.0, -1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0)
 
