a1004924ce95da3766bb5206b2a38578e031e023
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2022-07-12 00:31:31 +0200

    doc: tweak is_selected and is_sensitive
    False -> false, and true -> a true value, because no guarantee is given on the type of the returned value
    Also, a list of actions is accepted

--- renpy/display/behavior.py
+++ renpy/display/behavior.py
@@ -387,7 +387,8 @@ def is_selected(action):
     :name: renpy.is_selected
     :doc: run
 
-    Returns true if `action` indicates it is selected, or false otherwise.
+    Returns a true value if the provided action or list of actions
+    indicates it is selected, and false otherwise.
     """
 
     if isinstance(action, (list, tuple)):
@@ -407,7 +408,8 @@ def is_sensitive(action):
     :name: renpy.is_sensitive
     :doc: run
 
-    Returns true if `action` indicates it is sensitive, or False otherwise.
+    Returns a true value if the provided action or list of actions
+    indicates it is sensitive, and false otherwise.
     """
 
     if isinstance(action, (list, tuple)):
