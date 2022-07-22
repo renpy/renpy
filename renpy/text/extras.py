775d291fec9720ac45a2eefb297f7f13a5e137d8
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2022-07-22 15:19:29 +0200

    doc: tweaks to last
    Add a label to avoid confusion between the statements (the first happens at init, the second at runtime), and fix wording.

--- renpy/text/extras.py
+++ renpy/text/extras.py
@@ -236,7 +236,9 @@ class ParameterizedText(object):
     different style properties. For example, one can write::
 
         image top_text = ParameterizedText(xalign=0.5, yalign=0.0)
-        show top_text "This text is shown at center-upper"
+
+        label start:
+            show top_text "This text is shown at the center-top of the screen"
     """
 
     def __init__(self, style='default', **properties):
