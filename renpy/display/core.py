038e86bedbaf029e2d26d2bcd9fbc71416580732
Asriel Senna <aanonyme.personne@hotmail.fr>
2022-09-24 19:00:38 +0200

    bare except

--- renpy/display/core.py
+++ renpy/display/core.py
@@ -3197,7 +3197,7 @@ class Interface(object):
             if renpy.android:
                 try:
                     android.activity.finishAndRemoveTask()
-                except:
+                except Exception:
                     pass
 
                 from jnius import autoclass
