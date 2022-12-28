renpy/renpy

commit 7d31bed4f62d4d47a0968f13060f2f66b13e5de5

text: Optimize tokenizing text without tags or newlines.

renpy/text/textsupport.pyx
@@ -109,6 +109,10 @@ def tokenize(unicode s):
         else:
             rv.append((TEXT, buf))
 
+    if (u"{" not in s) and (u'\n' not in s) and (u"【" not in s):
+        rv.append((TEXT, s))
+        return rv
+
     for c in s:
 
         if state == TEXT_STATE:

commit e020f225f863759c501eae0ccfea25109bb5006b

Fix regression in how Text("") was rendered.
7d31bed4f62d4d47a0968f13060f2f66b13e5de5 introduced an issue that
changed how the empty string renders. This restores the longstanding
behavior.

renpy/text/textsupport.pyx
@@ -109,6 +109,9 @@ def tokenize(unicode s):
         else:
             rv.append((TEXT, buf))
 
+    if not s:
+        return [ ]
+
     if (u"{" not in s) and (u'\n' not in s) and (u"【" not in s):
         rv.append((TEXT, s))
         return rv
@@ -167,6 +170,9 @@ def tokenize(unicode s):
 
     if buf:
         finish_text()
+
+    if s == "":
+        print(rv)
 
     return rv
 