ae40b4922e19aaeecd7c90f5c4b9ce9a1f21e052
Mal Graty <mal.graty@googlemail.com>
2023-01-02 06:15:09 +0000

    Group layer definitions in config.py

--- renpy/config.py
+++ renpy/config.py
@@ -166,6 +166,9 @@ context_clear_layers = [ 'screens', 'top', 'bottom' ]
 # not participate in transitions.
 top_layers = [ 'top' ]
 
+# Layers below all other layers, that do not participate in transitions.
+bottom_layers = [ 'bottom' ]
+
 # True if we want to show overlays during wait statements, or
 # false otherwise.
 overlay_during_with = True
@@ -1295,9 +1298,6 @@ lenticular_bracket_ruby = True
 
 # If true, the web implentation of renpy.input will be used.
 web_input = True
-
-# Layers below all other layers, that do not participate in transitions.
-bottom_layers = [ 'bottom' ]
 
 # Aliases for the keys on the numeric keypad, to make them easier to write as keysyms.
 key_aliases = {
