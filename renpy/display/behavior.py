9c197abca38af634273b534db37b516d0b587d87
Kassy <78169097+Kassy2048@users.noreply.github.com>
2023-03-26 00:26:00 +0100

    Cancel long press wait when user stops touching the screen

--- renpy/display/behavior.py
+++ renpy/display/behavior.py
@@ -1115,6 +1115,9 @@ class Button(renpy.display.layout.Window):
 
                 renpy.game.interface.timeout(renpy.config.longpress_duration)
 
+            elif ev.type == pygame.MOUSEBUTTONUP and ev.button == 1:
+                self.longpress_start = None
+
             if self.longpress_start is not None:
                 if math.hypot(x - self.longpress_x, y - self.longpress_y) > renpy.config.longpress_radius:
                     self.longpress_start = None
