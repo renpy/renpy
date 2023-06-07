b0b096bf1635ed98f9b26e3dc4ae7cabdb7f884b
Tom Rothamel <pytom@bishoujo.us>
2023-06-07 01:03:48 -0400

    audio: if_changed handles tight looping.
    Subject to how if_changed works, as it does queue up a new
    loop.
    
    Fixes #4695.

--- renpy/audio/ioshw.py
+++ renpy/audio/ioshw.py
@@ -163,7 +163,7 @@ class IOSVideoChannel(object):
         self.stop()
         self.queue = [ ]
 
-    def enqueue(self, filenames, loop=True, synchro_start=False, fadein=0, tight=None, loop_only=False, relative_volume=1.0):
+    def enqueue(self, filenames, loop=True, synchro_start=False, fadein=0, tight=None, relative_volume=1.0):
         self.queue.extend(filenames)
 
     def pause(self):
