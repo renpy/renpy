3ff08b9c03ed34666f967add158eadc005dc3b65
Tom Rothamel <pytom@bishoujo.us>
2023-09-05 22:06:47 -0400

    Produce a valid-ish version for pygame_sdl2.

--- distribute.py
+++ distribute.py
@@ -252,7 +252,7 @@ def main():
             "-q",
             "egg_info",
             "--tag-build",
-            "-for-renpy-" + args.version,
+            "+renpy" + args.version,
             "sdist",
             "-d",
             os.path.abspath(destination)
