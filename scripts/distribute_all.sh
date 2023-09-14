057f69602b278d423b06379adb0723e5a0548597
Tom Rothamel <pytom@bishoujo.us>
2023-09-14 17:57:59 -0400

    Avoid nicing distribute.

--- scripts/distribute_all.sh
+++ scripts/distribute_all.sh
@@ -14,8 +14,8 @@ sleep 2
     ./build.sh
 )
 
-nice rpy build --python 3
-nice rpy build --python 2
+rpy build --python 3
+rpy build --python 2
 
-nice lib/py3-linux-x86_64/python distribute.py
-nice lib/py2-linux-x86_64/python distribute.py
+lib/py3-linux-x86_64/python distribute.py
+lib/py2-linux-x86_64/python distribute.py
