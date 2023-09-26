b2893b5ea8fc3a7855ef7c9f50f11d25a10c0397
Tom Rothamel <pytom@bishoujo.us>
2023-09-25 23:58:33 -0400

    rpu: Create plan.
    The plan decides where to get data from, and write it to.

--- experimental/renpyupdate/update.py
+++ experimental/renpyupdate/update.py
@@ -3,6 +3,39 @@ import os
 
 from . import filetypes
 
+class UpdateError(Exception):
+    """
+    Raised when an error happens.
+    """
+
+    pass
+
+class Plan(object):
+    """
+    This represents a plan for updating a single segment.
+    """
+
+    def __init__(self, block, source_filename, source_size, source_offset, compressed, target_filename, target_size, target_offset, hash):
+        # Is source_fn a blockfile that might need to be downloaded?
+        self.block = block
+
+        # The source filename, size, and offset.
+        self.source_filename = source_filename
+        self.source_size = source_size
+        self.source_offset = source_offset
+
+        # If true, the source information is compressed.
+        self.compressed = compressed
+
+        # The target filename, size, and offset.
+        self.target_filename = target_filename
+        self.target_size = target_size
+        self.target_offset = target_offset
+
+        # The hash.
+        self.hash = hash
+
+
 class Update(object):
 
     def __init__(self, rpudir, newlists, targetdir, oldlists):
@@ -13,11 +46,16 @@ class Update(object):
 
         self.new_files = [ i for j in self.newlists for i in j.files ]
         self.old_files = [ i for j in self.oldlists for i in j.files ]
+        self.block_files = [ i for j in self.newlists for i in j.blocks ]
+
+        # A list of plan objects.
+        self.plan = [ ]
 
         self.write_padding()
         self.find_incomplete_files()
         self.scan_old_files()
         self.remove_identical_files()
+        self.create_plan()
 
     def log(self, message, *args):
         print(message % args)
@@ -109,6 +147,50 @@ class Update(object):
         self.log("%d files are new/changed.", len(new_files))
 
         self.new_files = new_files
+
+
+    def create_plan(self):
+        """
+        Creates the plan for updating everything, by finding each missing
+        segment and creating a plan object.
+        """
+
+        # A map from segment hash to (block, file, segment) tuples.
+        segment_locations = { }
+
+        for f in sorted(self.block_files, key=lambda i : i.mtime):
+            for s in f.segments:
+                segment_locations[s.hash] = (True, f, s)
+
+        for f in sorted(self.old_files, key=lambda i : i.mtime):
+            for s in f.segments:
+                segment_locations[s.hash] = (False, f, s)
+
+        plan = [ ]
+
+        for target_file in self.new_files:
+            for target_segment in target_file.segments:
+                block, source_file, source_segment = segment_locations.get(target_segment.hash, (False, None, None))
+
+                if source_file is None:
+                    self.log("Segment %s was not found.", target_segment.hash)
+                    raise Exception("Segment %s was not found in index.")
+
+                plan.append(Plan(
+                    block,
+                    source_file.data_filename or source_file.name,
+                    source_segment.offset,
+                    source_segment.size,
+                    source_segment.compressed,
+                    target_file.name,
+                    target_segment.offset,
+                    target_segment.size,
+                    target_segment.hash,
+                    ))
+
+        plan.sort(key=lambda p : (p.block, p.source_filename, p.source_offset, p.source_size))
+
+        self.log("Created a plan with %d entries.", len(plan))
 
 
 def main():
