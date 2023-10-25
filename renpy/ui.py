0c9dd5bb37c23be8fc14e2d4848816a55eef2043
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2023-10-25 14:02:55 +0200

    Unused import

--- renpy/ui.py
+++ renpy/ui.py
@@ -29,8 +29,6 @@
 from __future__ import division, absolute_import, with_statement, print_function, unicode_literals
 from renpy.compat import PY2, basestring, bchr, bord, chr, open, pystr, range, round, str, tobytes, unicode # *
 
-from typing import Optional
-
 import sys
 
 import renpy
