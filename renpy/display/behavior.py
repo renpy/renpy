f3e85d6a24ff1246dc0739ac300e918dc56e7946
Tom Rothamel <pytom@bishoujo.us>
2023-11-13 01:08:43 -0500

    Add missing colon.

--- renpy/display/behavior.py
+++ renpy/display/behavior.py
@@ -2023,7 +2023,7 @@ class Adjustment(renpy.object.Object):
     def inertia_warper(self, done):
         if done < 0.0:
             done = 0.0
-        elif done > 1.0
+        elif done > 1.0:
             done = 1.0
 
         return 1.0 - math.exp(-done * 6)
