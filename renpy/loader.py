13cfd68491a38ca1d864c6b668b8c527e58923e2
Tom Rothamel <pytom@bishoujo.us>
2023-12-04 04:11:12 -0500

    Make the loader use RWopsIO.

--- renpy/loader.py
+++ renpy/loader.py
@@ -35,7 +35,7 @@ import re
 import io
 import unicodedata
 
-from pygame_sdl2.rwobject import RWops_from_file, RWops_create_subfile
+from pygame_sdl2.rwobject import RWopsIO
 
 from renpy.compat.pickle import loads
 from renpy.webloader import DownloadNeeded
@@ -448,181 +448,18 @@ def listdirfiles(common=True):
         return list(game_files)
 
 
-class SubFile(object):
-    closed = False
 
-    def __init__(self, fn, base, length, start):
-        self.fn = fn
-
-        self.f = None
-
-        self.base = base
-        self.offset = 0
-        self.length = length
-        self.start = start
-
-        if not self.start:
-            self.name = fn
-        else:
-            self.name = None
-
-    def open(self):
-        self.f = open(self.fn, "rb")
-        self.f.seek(self.base)
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, _type, value, tb):
-        self.close()
-        return False
-
-    def read(self, length=None):
-
-        if self.f is None:
-            self.open()
-
-        maxlength = self.length - self.offset
-
-        if length is not None:
-            length = min(length, maxlength)
-        else:
-            length = maxlength
-
-        rv1 = self.start[self.offset:self.offset + length]
-        length -= len(rv1)
-        self.offset += len(rv1)
-
-        if length:
-            rv2 = self.f.read(length)
-            self.offset += len(rv2)
-        else:
-            rv2 = b""
-
-        return (rv1 + rv2)
-
-    def readable(self):
-        return True
-
-    def readline(self, length=None):
-
-        if self.f is None:
-            self.open()
-
-        maxlength = self.length - self.offset
-        if length is not None:
-            length = min(length, maxlength)
-        else:
-            length = maxlength
-
-        # If we're in the start, then read the line ourselves.
-        if self.offset < len(self.start):
-            rv = b''
-
-            while length:
-                c = self.read(1) # type: bytes
-                rv += c
-                if c == b'\n':
-                    break
-                length -= 1
-
-            return rv
-
-        # Otherwise, let the system read the line all at once.
-        rv = self.f.readline(length)
-
-        self.offset += len(rv)
-
-        return rv
-
-    def readlines(self, length=None):
-        rv = [ ]
-
-        while True:
-            l = self.readline(length)
-
-            if not l:
-                break
-
-            if length is not None:
-                length -= len(l)
-                if length < 0:
-                    break
-
-            rv.append(l)
-
-        return rv
-
-    def seekable(self):
-        return True
-
-    def writable(self):
-        return False
-
-    def xreadlines(self):
-        return self
-
-    def __iter__(self):
-        return self
-
-    def __next__(self): # @ReservedAssignment
-        rv = self.readline()
-
-        if not rv:
-            raise StopIteration()
-
-        return rv
-
-    next = __next__
-
-    def flush(self):
-        return
-
-    def seek(self, offset, whence=0):
-
-        if self.f is None:
-            self.open()
-
-        if whence == 1:
-            offset = self.offset + offset
-        elif whence == 2:
-            offset = self.length + offset
-
-        if offset > self.length:
-            offset = self.length
-
-        self.offset = offset
-
-        offset = offset - len(self.start)
-        if offset < 0:
-            offset = 0
-
-        self.f.seek(offset + self.base)
-
-    def tell(self):
-        return self.offset
-
-    def close(self):
-        if self.f is not None:
-            self.f.close()
-            self.f = None
-
-    def write(self, s):
-        raise Exception("Write not supported by SubFile")
-
-
-open_file = RWops_from_file # type: ignore
+open_file = RWopsIO # type: ignore
 
 if "RENPY_FORCE_SUBFILE" in os.environ:
 
     def open_file(name, mode):
-        f = RWops_from_file(name, mode)
+        with RWopsIO(name, mode) as f:
+            f.seek(0, 2)
+            length = f.tell()
+            f.seek(0, 0)
 
-        f.seek(0, 2)
-        length = f.tell()
-        f.seek(0, 0)
-
-        return RWops_create_subfile(f, 0, length)
+        return RWopsIO(name, base=0, length=length)
 
 # A list of callbacks to open an open python file object of the given type.
 file_open_callbacks = [ ]
@@ -718,10 +555,11 @@ def load_from_archive(name):
                 offset, dlen, start = t
 
             if start == None or len(start) == 0:
-                rw = RWops_from_file(afn, "rb")
-                rv = RWops_create_subfile(rw, offset, dlen)
+                rv = RWopsIO(afn, "rb", base=offset, length=dlen)
+                return io.BufferedReader(rv)
             else:
-                rv = SubFile(afn, offset, dlen, start)
+                raise ValueError("Subfile with start temporarily not supported.")
+                # rv = SubFile(afn, offset, dlen, start)
 
         # Compatibility path.
         else:
@@ -730,9 +568,7 @@ def load_from_archive(name):
                     f.seek(offset)
                     data.append(f.read(dlen))
 
-                rv = io.BytesIO(b''.join(data))
-
-        return rv
+                return io.BytesIO(b''.join(data))
 
     return None
 
