25ec9105bb9dacbf867597af1c392695465f879f
Tom Rothamel <pytom@bishoujo.us>
2023-12-10 20:00:04 -0500

    Browser shouldn't start in fullscreen by default.

--- renpy/display/core.py
+++ renpy/display/core.py
@@ -961,6 +961,9 @@ class Interface(object):
         if self.started:
             return
 
+        if renpy.emscripten:
+            renpy.game.preferences.fullscreen = False
+
         # Avoid starting on Android if we don't have focus.
         if renpy.android:
             self.check_android_start()
