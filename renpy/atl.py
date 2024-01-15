89ab6bc81ffc5e15b32874b7ca4309123d293935
Mal Graty <mal.graty@googlemail.com>
2024-01-15 22:44:24 +0000

    Revert "Simplify ALIASES and nuke dualangle_or_float_or_none"
    This reverts commit 8d5b1b55c05992fdd64a9c3056631747c9ab0286.

--- renpy/atl.py
+++ renpy/atl.py
@@ -173,6 +173,12 @@ def position_or_none(x):
     return position.from_any(x)
 
 
+def dualangle_or_float_or_none(x):
+    if x is None:
+        return None
+    return DualAngle.from_any(x)
+
+
 def any_object(x):
     return x
 
@@ -1589,7 +1595,7 @@ class Interpolation(Statement):
         if anchorangles is not None:
             startangle, endangle = anchorangles[:2]
 
-            anchorangle = interpolate(complete, startangle, endangle, DualAngle.from_any)
+            anchorangle = interpolate(complete, startangle, endangle, dualangle_or_float_or_none)
             trans.state.anchorangle = anchorangle
 
         if anchorradii is not None:
