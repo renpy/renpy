7bf86d6851087e2c14cd9829545ec0b7645d20ef
Tom Rothamel <pytom@bishoujo.us>
2024-02-14 01:06:17 -0500

    When processing at-lists, check for ATL after transforms.

--- renpy/display/core.py
+++ renpy/display/core.py
@@ -2579,9 +2579,11 @@ class Interface(object):
 
                 if isinstance(a, renpy.display.motion.Transform):
                     rv = a(child=rv)
-                    new_transform = rv
                 else:
                     rv = a(rv)
+
+                if isinstance(rv, renpy.display.transform.Transform):
+                    new_transform = rv
 
             if (new_transform is not None):
                 scene_lists.transform_state(self.old_root_transform, new_transform, execution=True)
