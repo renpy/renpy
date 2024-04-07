f9ccc4b2e01d0fff068695e569bf5b0799ef0792
Tom Rothamel <pytom@bishoujo.us>
2024-04-06 22:15:05 -0400

    Generate string translations from _ren.py files.
    Fixes #5458.

--- renpy/translation/generation.py
+++ renpy/translation/generation.py
@@ -224,6 +224,9 @@ def translation_filename(s):
 
     filename = s.elided
 
+    if filename.endswith("_ren.py"):
+        filename = filename[:-7] + ".rpy"
+
     if filename[-1] == "m":
         filename = filename[:-1]
 
@@ -441,7 +444,7 @@ def translate_list_files():
 
         filename = os.path.join(dirname, filename)
 
-        if not (filename.endswith(".rpy") or filename.endswith(".rpym")):
+        if not (filename.endswith(".rpy") or filename.endswith(".rpym") or filename.endswith("_ren.py")):
             continue
 
         filename = os.path.normpath(filename)
