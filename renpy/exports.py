ff8e0a0cb062b8367f1c1050626f20b2edaa03fa
Tom Rothamel <pytom@bishoujo.us>
2024-04-22 21:45:45 -0400

    Ensure the vibration time is above a minimum value.
    Fixes #5474.

--- renpy/exports.py
+++ renpy/exports.py
@@ -3603,6 +3603,9 @@ def vibrate(duration):
     is only supported on Android.
     """
 
+    if duration < 0.01:
+        duration = 0.01
+
     if renpy.android:
         import android # @UnresolvedImport
         android.vibrate(duration)
