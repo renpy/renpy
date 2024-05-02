88246afd66abd75adef6e7ac64487cb544934b5c
Tom Rothamel <pytom@bishoujo.us>
2024-05-02 01:47:27 -0400

    Add RENPY_REPORT_EXCEPTIONS.
    This is mostly intended for use in VS Code and similar environments.
    When an exception happens, this will propagate it through Ren'Py,
    allowing the debugger to inspect the state of the game.

--- renpy/error.py
+++ renpy/error.py
@@ -168,6 +168,9 @@ def report_exception(e, editor=True):
     # Note: Doki Doki Literature club calls this as ("Words...", False).
     # For what it's worth.
 
+    if not int(os.environ.get("RENPY_REPORT_EXCEPTIONS", "1")):
+        raise
+
     # The sound system may not be ready during exception handling.
     renpy.config.debug_sound = False
 
