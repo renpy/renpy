ea5bffc7995460d64c5ce059a25a8f3f7a08290c
Tom Rothamel <pytom@bishoujo.us>
2024-08-06 22:18:47 -0400

    Name Ren'Py 8.

--- renpy/versions.py
+++ renpy/versions.py
@@ -48,8 +48,8 @@ class Version(object):
 
         py_branch_to_version[(python, branch)] = self
 
-Version("main", 3, "8.3.0", "TBD")
-Version("main", 2, "7.8.0", "TBD")
+Version("main", 3, "8.3.0", "Second Star to the Right")
+Version("main", 2, "7.8.0", "Straight on Till Morning")
 
 Version("fix", 3, "8.2.4", "64bit Sensation")
 Version("fix", 2, "7.7.4", "32bit Sensation")
