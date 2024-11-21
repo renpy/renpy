3f93c5a1406dce6105cae9acb6b05b34de7ee459
Mal Graty <mal.graty@googlemail.com>
2024-11-21 13:43:01 +0000

    input: Improve rendering of caret
    This change ensures that there is always space in the input render for
    the caret, while retaining the current behaviour where shifting the
    caret left and right does not impact the layout of the text.
    
    This allows us to go back to using a zero-width space as a sentinel in
    empty inputs (to ensure their correct height) thus avoiding ambiguity
    between a sentinel space and a user-provided space.

--- renpy/text/text.py
+++ renpy/text/text.py
@@ -508,9 +508,6 @@ class DisplayableSegment(object):
 
         self.width, self.height = rend.get_size()
 
-        if isinstance(d, renpy.display.behavior.CaretBlink):
-            self.width = 0
-
         self.hyperlink = ts.hyperlink
         self.cps = ts.cps
         self.ruby_top = ts.ruby_top
@@ -530,7 +527,7 @@ class DisplayableSegment(object):
         glyph.character = 0xfffc
         glyph.ascent = 0
         glyph.line_spacing = h
-        glyph.advance = w
+        glyph.advance = 0 if isinstance(self.d, renpy.display.behavior.CaretBlink) else w
         glyph.width = w
         glyph.shader = self.shader
 
@@ -1227,7 +1224,7 @@ class Layout(object):
                 if isinstance(i[0], (TextSegment, SpaceSegment, DisplayableSegment)):
                     return
 
-            line.extend(tss[-1].subsegment(u"\u200B")) # type: ignore
+            line.extend(tss[-1].subsegment("\u200b")) # type: ignore
 
         for type, text in tokens: # @ReservedAssignment
 
@@ -1246,9 +1243,8 @@ class Layout(object):
 
                 elif type == TEXT:
 
-                    if (text_displayable.mask is not None):
-                        if text != " ":
-                            text = text_displayable.mask * len(text)
+                    if text_displayable.mask is not None and text != "\u200b":
+                        text = text_displayable.mask * len(text)
 
                     line.extend(self.create_text_segments(text, tss[-1], style))
 
