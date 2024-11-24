864feaba022da4e58fab0db419618da5cd13729c
Tom Rothamel <pytom@bishoujo.us>
2024-11-24 11:48:50 -0500

    Indentation fix from #5947.

--- renpy/display/behavior.py
+++ renpy/display/behavior.py
@@ -1116,9 +1116,9 @@ class Button(renpy.display.layout.Window):
         # If we have a child, try passing the event to it. (For keyboard
         # events, this only happens if we're focused.)
         if (not (ev.type in KEY_EVENTS)) or self.style.key_events:
-                rv = super(Button, self).event(ev, x, y, st)
-                if rv is not None:
-                    return rv
+            rv = super(Button, self).event(ev, x, y, st)
+            if rv is not None:
+                return rv
         else:
 
             # Used to prevent keymaps (the key statement) from reacting to
