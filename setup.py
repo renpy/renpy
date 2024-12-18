fd5841d2d25b5ef8cfa4fd09617529f0be6bf0f9
Andy_kl <andy_kl@bk.ru>
2024-12-19 01:00:29 +0400

    Delete lexersupport module.

--- setup.py
+++ setup.py
@@ -91,7 +91,7 @@ def main():
     # renpy
     cython("renpy.astsupport")
     cython("renpy.cslots")
-    cython("renpy.lexersupport")
+    cython("renpy.location")
     cython("renpy.pydict")
     cython("renpy.style")
     cython("renpy.encryption")
