61839cd622f1454ea635e4ed2ced09ec1fdb3f62
Arsym <123092893+arsym-dev@users.noreply.github.com>
2025-06-20 16:11:01 -0400

    Dotted testcase names
    Testcases and testsuites can be added to a testsuite by using a dotted name (eg. `testcase default.unit`) as long as the parent is a testsuite.

--- renpy/test/testparser.py
+++ renpy/test/testparser.py
@@ -353,7 +353,7 @@ def testsuite_statement(l: Lexer, loc: NodeLocation) -> testast.TestSuite:
     after: testast.Block | None = None
     children: list[testast.TestCase] = [ ]
 
-    name = l.require(l.name)
+    name = l.require(l.dotted_name)
     signature = renpy.parser.parse_parameters(l)
 
     if name == "all" and current_testsuite_name:
@@ -445,7 +445,7 @@ def testcase_statement(l: Lexer, loc: NodeLocation) -> testast.TestCase:
 
     global current_testsuite_name
 
-    name = l.require(l.name)
+    name = l.require(l.dotted_name)
     signature: renpy.parameter.Signature | None = renpy.parser.parse_parameters(l)
 
     if name == "all":
