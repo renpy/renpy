47090d74b25b3cc48000e8f3498e648469b559f0
Arsym <123092893+arsym-dev@users.noreply.github.com>
2025-06-19 20:13:07 -0400

    Moved exceptions to types.py

--- renpy/test/types.py
+++ renpy/test/types.py
@@ -3,3 +3,7 @@ from typing import TypeAlias, Any
 State: TypeAlias = Any
 NodeLocation: TypeAlias = tuple[str, int]
 Position: TypeAlias = tuple[int | float, int | float]
+
+class RenpyTestException(RuntimeError):pass
+class RenpyTestAssertionError(AssertionError):pass
+class RenpyTestTimeoutError(TimeoutError):pass
