5a746d028b21ab0ad502365b4b4f982974eb92cb
Tom Rothamel <pytom@bishoujo.us>
2025-10-25 21:12:34 -0400

    Add name.

--- renpy/versions.py
+++ renpy/versions.py
@@ -49,7 +49,7 @@ class Version(object):
         py_branch_to_version[(python, branch)] = self
 
 
-Version("main", 3, "8.5.0", "TBD")
+Version("main", 3, "8.5.0", "In Good Health")
 
 Version("fix", 3, "8.4.2", "Tomorrowland")
 
