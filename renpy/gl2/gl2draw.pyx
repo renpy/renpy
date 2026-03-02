3c02e4997f8ce9c18b4ce35368ae278305b78eac
Tom Rothamel <pytom@bishoujo.us>
2026-03-01 22:04:57 -0500

    sdl3: Avoid re-creating the GL context on Emscripten.
    This is no longer allowed by the new emscripten. This undoes a fix for
    bug #6515 - hopefully the new emscripten solves the underlying issue.

--- renpy/gl2/gl2draw.pyx
+++ renpy/gl2/gl2draw.pyx
@@ -537,7 +537,7 @@ cdef class GL2Draw:
                 self.shader_cache.clear()
 
         if full_reset:
-            if pygame.display.get_window().recreate_gl_context(always=renpy.emscripten):
+            if pygame.display.get_window().recreate_gl_context():
                 renpy.display.interface.kill_textures()
 
         # Are we in fullscreen mode?
