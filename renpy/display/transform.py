92b5a2b35b5e985b03de7e7066f789db44396306
Tom Rothamel <pytom@bishoujo.us>
2026-03-17 00:51:09 -0400

    Run a transform before _in_current_store-ing it.
    To ensure its child exists.
    
    Fixes #6945.

--- renpy/display/transform.py
+++ renpy/display/transform.py
@@ -1220,6 +1220,10 @@ class Transform(Container):
         return rv
 
     def _in_current_store(self):
+
+        if not self.active:
+            self.update_state()
+
         if self.child is None:
             return self
 
