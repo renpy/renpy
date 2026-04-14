2197e966a3a6ee5fa1939e4d737aadb31920c06f
Tom Rothamel <pytom@bishoujo.us>
2026-04-13 21:57:28 -0400

    sphinx: Include release version in documentation.
    The release version is the one that's likely to be available on the
    website. It's used in the Chrome OS documentation, so we try to
    derive it from the version of the fix branch, if we're on a nightly release of the fix branch.

--- renpy/versions.py
+++ renpy/versions.py
@@ -112,6 +112,25 @@ def get_version():
     return make_dict(branch)
 
 
+def get_release_version():
+    """
+    Returns the version of the last release. If this is a nightly release, returns the version of the fix branch,
+    minus one. (eg, if the fix branch is 8.5.3, returns 8.5.2). Otherwise, returns the version of the main branch,
+    without the suffix.
+
+    This is only intended for development use from a git checkout.
+    """
+
+    version = get_version()
+    version_tuple = tuple(map(int, version["version"].split(".")[:3]))
+
+    if version["nightly"] and version["branch"] == "fix" and version_tuple[2] > 0:
+            return "{}.{}.{}".format(version_tuple[0], version_tuple[1] - 1, version_tuple[2])
+    else:
+        return "{}.{}.{}".format(*version_tuple)
+
+
+
 def generate_vc_version(nightly=False):
     """
     Generates the vc_version.py file.
